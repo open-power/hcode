bfdeb06f99dd96f7e0bac0b6a56b4e5b3e218a8b
Martin Peschke <mpeschke@de.ibm.com>
2016-03-23 12:52:58 +0100

    Self-Restore Image Build Flow
    Change-Id: I68f9d98aa94223d6ed56e0c16a5ab2a6a610505c
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/22386
    Tested-by: Jenkins Server
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Martin Peschke <mpeschke@de.ibm.com>

---
+++ import/tools/imageProcs/restore_image.cmd
@@ -0,0 +1,105 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/tools/imageProcs/restore_image.cmd $                   */
+/*                                                                        */
+/* OpenPOWER HCODE Project                                                */
+/*                                                                        */
+/* COPYRIGHT 2016,2017                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+// Need to do this so that elf32-powerpc is not modified!
+#undef powerpc
+
+#define RESTORE_IMAGE_ORIGIN 0x80000000
+OUTPUT_FORMAT(elf32-powerpc);
+
+SECTIONS
+{
+    . = RESTORE_IMAGE_ORIGIN;
+     _restore_image_origin = . - 0;
+    ////////////////////////////////
+    // Header
+    ////////////////////////////////
+    . = ALIGN(1);
+    _header_origin = .;
+    _header_offset = . - _restore_image_origin;
+    .header . : { *(.header) }
+    _header_size = . - _header_origin;
+
+    ////////////////////////////////
+    // TOC
+    ////////////////////////////////
+    . = ALIGN(4);
+    _toc_origin = .;
+    _toc_offset = . - _restore_image_origin;
+    .toc . : { *(.toc) }
+    _toc_size = . - _toc_origin;
+
+    ////////////////////////////////
+    // STRING
+    ////////////////////////////////
+    . = ALIGN(1);
+    _strings_origin = .;
+    _strings_offset = . - _restore_image_origin;
+    .strings . : { *(.strings) }
+    _strings_size = . - _strings_origin;
+
+    ////////////////////////////////
+    // CPMR
+    ////////////////////////////////
+    . = ALIGN(8);
+    _cpmr_origin = .;
+    _cpmr_offset = . - _restore_image_origin;
+    .cpmr . : { *(.cpmr) }
+    _cpmr_size = . - _cpmr_origin;
+
+    ////////////////////////////////
+    // Self-Restore
+    ////////////////////////////////
+    . = ALIGN(8);
+    _self_restore_origin = .;
+    _self_restore_offset = . - _restore_image_origin;
+    .self_restore . : { *(.self_restore) }
+    _self_restore_size = . - _self_restore_origin;
+
+    ////////////////////////////////
+    // SPR Restore
+    ////////////////////////////////
+    . = ALIGN(8);
+    _spr_restore_origin = .;
+    _spr_restore_offset = . - _restore_image_origin;
+    .spr_restore . : { *(.spr_restore) }
+    _spr_restore_size = . - _spr_restore_origin;
+
+    ////////////////////////////////
+    // SCOM Restore
+    ////////////////////////////////
+    . = ALIGN(8);
+    _scom_restore_origin = .;
+    _scom_restore_offset = . - _restore_image_origin;
+    .scom_restore . : { *(.scom_restore) }
+    _scom_restore_size = . - _scom_restore_origin;
+
+    ////////////////////////////////
+    // end of the image
+    ////////////////////////////////
+    . = ALIGN(8);
+    _restore_image_size = . - _restore_image_origin;
+    _restore_image_end = . - 0;
+}
