5ed8f98bf4e99c6fc00dfb10469f78715fbeff45
Amit Kumar <akumar3@us.ibm.com>
2016-09-23 08:25:20 -0500

    CMEedit
    fixed an issue related to circular dependency
    Also removed some libs that were not needed
    
    Change-Id: I06c85c9ad5ec102d80dc9b796601547bc3d7f7fd
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/30166
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- import/chips/p9/procedures/ppe_closed/cme/stop_cme/p9_cme_edit.mk
+++ import/chips/p9/procedures/ppe_closed/cme/stop_cme/p9_cme_edit.mk
@@ -24,27 +24,14 @@
 # IBM_PROLOG_END_TAG
 EXE=cmeImgEdit
 
-CME_LIB=$(ROOTPATH)/chips/p9/procedures/ppe_closed/cme/
-HCODE_UTIL=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/xip/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/hwp/lib/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/ppe_closed/lib/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/utils/imageProcs/
-HCODE_UTIL+=$(ROOTPATH)/tools/imageProcs/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/ppe/pk/kernel
-$(EXE)_DEPLIBS+=p9_xip_image
-$(EXE)_DEPLIBS+=p9_ringId
-
-$(IMAGE)_COMMONFLAGS+= -DPK_TIMER_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DPK_THREAD_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DPK_TRACE_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DUSE_PK_APP_CFG_H=1
-$(IMAGE)_COMMONFLAGS+= -D__PK__=1
 $(EXE)_COMMONFLAGS+= -D__PPE_PLAT
 
-$(call ADD_EXE_INCDIR,$(EXE), $(HCODE_UTIL))
-$(call ADD_EXE_INCDIR,$(EXE), $(CME_LIB))
+$(call ADD_EXE_INCDIR, $(EXE), \
+   $(CME_SRCDIR) \
+   $(PK_SRCDIR)/kernel \
+   $(HCODE_COMMON_LIBDIR) \
+   $(HCODE_LIBDIR) \
+    ) 
 
 
 IMAGE_DEPS+=cmeImgEdit
@@ -53,28 +40,14 @@ $(call BUILD_EXE)
 
 EXE=cpmr_headerImgEdit
 
-CME_LIB=$(ROOTPATH)/chips/p9/procedures/ppe_closed/cme/
-HCODE_UTIL=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/xip/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/hwp/lib/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/ppe_closed/lib/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/utils/imageProcs/
-HCODE_UTIL+=$(ROOTPATH)/tools/imageProcs/
-HCODE_UTIL+=$(ROOTPATH)/chips/p9/procedures/ppe/pk/kernel
-$(EXE)_DEPLIBS+=p9_xip_image
-$(EXE)_DEPLIBS+=p9_ringId
-
-$(IMAGE)_COMMONFLAGS+= -DPK_TIMER_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DPK_THREAD_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DPK_TRACE_SUPPORT=1
-$(IMAGE)_COMMONFLAGS+= -DUSE_PK_APP_CFG_H=1
-$(IMAGE)_COMMONFLAGS+= -D__PK__=1
 $(EXE)_COMMONFLAGS+= -D__PPE_PLAT
 
-$(call ADD_EXE_INCDIR,$(EXE), $(HCODE_UTIL))
-$(call ADD_EXE_INCDIR,$(EXE), $(CME_LIB))
-
+$(call ADD_EXE_INCDIR, $(EXE), \
+   $(CME_SRCDIR) \
+   $(PK_SRCDIR)/kernel \
+   $(HCODE_COMMON_LIBDIR) \
+   $(HCODE_LIBDIR) \
+    ) 
 
 
 IMAGE_DEPS+=cpmr_headerImgEdit
