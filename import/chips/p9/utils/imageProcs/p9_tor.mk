98fe04d0d6524aa02ac5e3132aabcd05dcefe6c5
Claus Michael Olsen <cmolsen@us.ibm.com>
2017-04-05 05:16:51 -0500

    Code restruct: TOR API
    Key_Cronus_Test=XIP_REGRESS
    
    Code restructuring aiming at:
    - utilizing TOR magic header info
    - enforce a common approach for
      - extracting metadata for all image,chipType combinations
      - traversing images for all image,chipType combinations
    - shrinking code size by reusing common code segments
    - improve readability by
      - separating more clearly metadata extraction and image traversal
      - slight rearrange of certain code segments
    - remove leftover hardcoded assumptions about ring/TOR data and structs
    - variables appropriately renamed and now all using camel style
    
    Change-Id: I50ace8b2fdb340a97ce6d74ce545c5e1acd21c40
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/38863
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Cronus HW CI <cronushw-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: GIRISANKAR PAULRAJ <gpaulraj@in.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- import/chips/p9/utils/imageProcs/p9_tor.mk
+++ import/chips/p9/utils/imageProcs/p9_tor.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER HCODE Project
 #
-# COPYRIGHT 2016,2017
+# COPYRIGHT 2016,2018
 # [+] International Business Machines Corp.
 #
 #
@@ -23,6 +23,7 @@
 #
 # IBM_PROLOG_END_TAG
 PROCEDURE=p9_tor
+lib$(PROCEDURE)_DEPLIBS += common_ringId
 lib$(PROCEDURE)_DEPLIBS += p9_ringId
 lib$(PROCEDURE)_DEPLIBS += cen_ringId
 $(call ADD_MODULE_INCDIR,$(PROCEDURE),$(ROOTPATH)/chips/centaur/utils/imageProcs)
--- import/chips/p9/xip/p9_xip_tool.mk
+++ import/chips/p9/xip/p9_xip_tool.mk
@@ -5,7 +5,7 @@
 #
 # OpenPOWER HCODE Project
 #
-# COPYRIGHT 2016,2017
+# COPYRIGHT 2016,2018
 # [+] International Business Machines Corp.
 #
 #
@@ -24,7 +24,7 @@
 # IBM_PROLOG_END_TAG
 EXE = p9_xip_tool
 OBJS += $(EXE).o
-$(EXE)_DEPLIBS += p9_xip_image p9_tor p9_ringId cen_ringId p9_scan_compression p9_dd_container
+$(EXE)_DEPLIBS += p9_xip_image common_ringId p9_tor p9_ringId cen_ringId p9_scan_compression p9_dd_container
 $(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/chips/p9/utils/imageProcs)
 $(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/chips/centaur/utils/imageProcs)
 $(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/chips/common/utils/imageProcs)
