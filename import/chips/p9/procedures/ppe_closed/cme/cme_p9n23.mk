52a1900d84390af89437d293405d1b1ea2446c68
Ben Gass <bgass@us.ibm.com>
2018-02-12 17:05:42 -0600

    Add support for p9c 1.2
    Also initial mk files for p9n 2.3, but p9c 1.2 will be first.
    
    Change-Id: Ibebeed2de246d4a7be34eca74c780c29bfddd688
    Original-Change-Id: Ia73aba37be5bcf64b1b2cfe5b1ed153b189c7777
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/53909
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: HWSV CI <hwsv-ci+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>
    Reviewed-by: James N. Klazynski <jklazyns@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ import/chips/p9/procedures/ppe_closed/cme/cme_p9n23.mk
@@ -0,0 +1,83 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p9/procedures/ppe_closed/cme/cme_p9n23.mk $
+#
+# OpenPOWER HCODE Project
+#
+# COPYRIGHT 2017,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+CME_IMAGE:=cme_p9n23
+IMAGE:=$(CME_IMAGE)
+IMAGE_EDITOR:=cmeImgEdit.exe
+
+#Select KERNEL
+$(IMAGE)_KERNEL:=__IOTA__
+#$(IMAGE)_KERNEL:=__PK__
+
+# Options for Platforms specific tuning
+
+
+$(IMAGE)_COMMONFLAGS = -DNIMBUS_DD_LEVEL=23
+$(IMAGE)_COMMONFLAGS+= -DCUMULUS_DD_LEVEL=0
+$(IMAGE)_COMMONFLAGS+= -DAXONE_DD_LEVEL=0
+
+$(IMAGE)_COMMONFLAGS+= -DPK_TRACE_LEVEL=1
+
+$(IMAGE)_COMMONFLAGS+= -DLAB_P9_TUNING=0
+$(IMAGE)_COMMONFLAGS+= -DEPM_P9_TUNING=0
+$(IMAGE)_COMMONFLAGS+= -DEPM_BROADSIDE_SCAN0=0
+
+$(IMAGE)_COMMONFLAGS+= -DSIMICS_TUNING=0
+$(IMAGE)_COMMONFLAGS+= -DUSE_SIMICS_IO=0
+
+include $(CME_SRCDIR)/cme_common.mk
+OBJS := $(CME_OBJS)
+
+$(call BUILD_PPEIMAGE)
+
+## ## Bin header
+ IMAGE=cpmr_header_p9n23
+ IMAGE_EDITOR=cmeImgEdit.exe
+##
+## # Target tool chain
+ $(IMAGE)_TARGET=PPE
+##
+## #linkscript to use
+ $(IMAGE)_LINK_SCRIPT=linkcpmr.cmd
+##
+ OBJS = stop_cme/p9_cme_cpmr.o
+
+$(call ADD_BINHEADER_INCDIR,$(IMAGE),\
+    $(CME_SRCDIR)/stop_cme \
+	$(CME_SRCDIR)/pstate_cme \
+	$(PK_SRCDIR)/kernel \
+	$(PK_SRCDIR)/ppe42 \
+	$(PK_SRCDIR)/trace \
+	$(PK_SRCDIR)/$(_PPE_TYPE) \
+	$(PM_LIBDIR)/include \
+	$(PM_LIBDIR)/include/registers \
+	$(PM_LIBDIR)/common \
+	$(HCODE_LIBDIR) \
+	$(HCODE_COMMON_LIBDIR) \
+	$(ROOTPATH)/chips/p9/procedures/hwp/lib/ \
+        $(ROOTPATH)/chips/p9/utils/imageProcs/ \
+	)
+
+ $(call BUILD_BINHEADER,$(IMAGEPATH)/$(CME_IMAGE)/$(CME_IMAGE).bin,$(ROOTPATH)/chips/p9/procedures/utils/stopreg/selfRest.bin)
