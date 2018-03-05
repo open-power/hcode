6ebb5508cb11657cf867aadb8391d6be2ed60b73
Rahul Batra <rbatra@us.ibm.com>
2018-03-05 10:00:41 -0600

    PM: Generated Vratio/Vindex tables
        Key_Cronus_Test=PM_REGRESS
    
    Change-Id: I3ecb8a4389c7b888c5323f2d8f013c3a17594cc5
    Original-Change-Id: I9313dbe90771a549e14c8e90f2c2ca410616293a
    CQ: SW421682
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/55059
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Tested-by: Cronus HW CI <cronushw-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- import/chips/p9/procedures/ppe_closed/pgpe/pstate_gpe/p9_pgpe_gen_pstate_info.c
+++ import/chips/p9/procedures/ppe_closed/pgpe/pstate_gpe/p9_pgpe_gen_pstate_info.c
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER HCODE Project                                                */
 /*                                                                        */
-/* COPYRIGHT 2016,2017                                                    */
+/* COPYRIGHT 2016,2018                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -28,6 +28,7 @@
 #include "p9_pgpe_gppb.h"
 #include "p9_pgpe_header.h"
 #include "pstate_pgpe_occ_api.h"
+#include "p9_pgpe.h"
 
 //Externs
 extern PgpeHeader_t* G_pgpe_header_data;
@@ -37,14 +38,21 @@ extern GlobalPstateParmBlock* G_gppb;
     GeneratedPstateInfo* G_gpi;
 #elif USE_GEN_PSTATE_STRUCT_V == 1
     GeneratedPstateInfo_v1* G_gpi;
+#elif USE_GEN_PSTATE_STRUCT_V == 2
+    GeneratedPstateInfo_v2* G_gpi;
 #endif
+
+compile_assert(GEN_PSTATE_INFO_CHECK, sizeof(GeneratedPstateInfo) < PGPE_PSTATE_OUTPUT_TABLES_REGION_SIZE);
+compile_assert(GEN_PSTATE_INFO_V1_CHECK, sizeof(GeneratedPstateInfo_v1) < PGPE_PSTATE_OUTPUT_TABLES_REGION_SIZE);
+compile_assert(GEN_PSTATE_INFO_V2_CHECK, sizeof(GeneratedPstateInfo_v2) < PGPE_PSTATE_OUTPUT_TABLES_REGION_SIZE);
 
 //
 //Local function prototypes
 //
-void p9_pgpe_gen_raw_pstates();
-void p9_pgpe_gen_biased_pstates();
+void p9_pgpe_gen_raw_pstates(PstateTable* tbl);
+void p9_pgpe_gen_biased_pstates(PstateTable* tbl);
 void p9_pgpe_gen_occ_pstate_tbl();
+void p9_pgpe_gen_vratio_vindex_tbl(VRatioVIndexTable* tbl);
 
 //
 //p9_pgpe_gen_pstate_info
@@ -89,6 +97,41 @@ void p9_pgpe_gen_pstate_info()
     PK_TRACE_DBG("INIT:highest_ps=0x%x, ps0=0x%x",
                  *highest_pstate,
                  *ps0);
+#elif USE_GEN_PSTATE_STRUCT_V == 2
+    G_gpi = (GeneratedPstateInfo_v2*)G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset;
+    G_gpi->magic = GEN_PSTATES_TBL_MAGIC_V2;
+    G_gpi->header.gppb_offset = offsetof(GeneratedPstateInfo_v2, globalppb) >> 2;
+    G_gpi->header.gppb_length = sizeof(GlobalPstateParmBlock);
+    G_gpi->header.ps0_offset  = offsetof(GeneratedPstateInfo_v2, pstate0_frequency_khz) >> 2;
+    G_gpi->header.highest_ps_offset = offsetof(GeneratedPstateInfo_v2, highest_pstate) >> 2;
+    G_gpi->header.raw_pstate_tbl_offset = offsetof(GeneratedPstateInfo_v2, raw_pstates) >> 2;
+    G_gpi->header.raw_pstate_tbl_length = sizeof(PstateTable) * MAX_PSTATE_TABLE_ENTRIES;
+    G_gpi->header.biased_pstate_tbl_offset = offsetof(GeneratedPstateInfo_v2, biased_pstates) >> 2;
+    G_gpi->header.biased_pstate_tbl_length = sizeof(PstateTable) * MAX_PSTATE_TABLE_ENTRIES;
+    G_gpi->header.vratio_vindex_tbl_offset = offsetof(GeneratedPstateInfo_v2, vratio_vindex) >> 2;
+    G_gpi->header.vratio_vindex_tbl_length = sizeof(VRatioVIndexTable) * MAX_VRATIO_VINDEX_TABLE_ENTRIES;
+
+    PK_TRACE_DBG("INIT:gppb=0x%x, ps0=0x%x",
+                 G_gpi->header.gppb_offset,
+                 G_gpi->header.ps0_offset);
+    PK_TRACE_DBG("INIT:highest_ps=0x%x, biased_ps=0x%x",
+                 G_gpi->header.highest_ps_offset,
+                 G_gpi->header.biased_pstate_tbl_offset);
+    PK_TRACE_DBG("INIT:vratio_vindex ofsset=0x%x,length=0x%x ",
+                 G_gpi->header.vratio_vindex_tbl_offset,
+                 G_gpi->header.vratio_vindex_tbl_length);
+    gppb = (GlobalPstateParmBlock*)((G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset + G_gpi->header.gppb_offset));
+    ps0 = (uint32_t*)(G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset + G_gpi->header.ps0_offset);
+    highest_pstate = (uint32_t*)(G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset + G_gpi->header.highest_ps_offset);
+    rTbl = (PstateTable*)(G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset + G_gpi->header.raw_pstate_tbl_offset);
+    bTbl = (PstateTable*)(G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset + G_gpi->header.biased_pstate_tbl_offset);
+    VRatioVIndexTable* vTbl = (VRatioVIndexTable*)(G_pgpe_header_data->g_pgpe_gen_pstables_mem_offset +
+                              G_gpi->header.vratio_vindex_tbl_offset);
+    PK_TRACE_DBG("INIT:highest_ps=0x%x, ps0=0x%x",
+                 *highest_pstate,
+                 *ps0);
+
+    p9_pgpe_gen_vratio_vindex_tbl(vTbl);
 #endif
 
     PK_TRACE_DBG("INIT:G_gpi=0x%x, rTbl=0x%x,bTbl=0x%x", (uint32_t)G_gpi, (uint32_t)rTbl, (uint32_t)bTbl);
@@ -102,6 +145,7 @@ void p9_pgpe_gen_pstate_info()
 
     //Generate Pstate table for OCC in SRAM
     p9_pgpe_gen_occ_pstate_tbl();
+
 }
 
 //
@@ -181,4 +225,33 @@ void p9_pgpe_gen_occ_pstate_tbl()
     }
 
     PK_TRACE_DBG("INIT: Generated OCC Tbl");
+}
+
+
+void p9_pgpe_gen_vratio_vindex_tbl(VRatioVIndexTable* tbl)
+{
+    uint32_t ac, sc, idx = 0;
+
+    PK_TRACE_DBG("INIT: Generating Vindex Vratio Tbl");
+
+    for (sc = 1; sc <= 24; sc++)
+    {
+        for (ac = 1; ac <= sc; ac++)
+        {
+            tbl[idx].vratio = (ac * MAX_VRATIO) / sc;
+
+            if (ac != 0)
+            {
+                tbl[idx].vindex = (((24 * tbl[idx].vratio) + VRATIO_ROUNDING_ADJUST) / MAX_VRATIO) - 1;
+            }
+            else
+            {
+                tbl[idx].vindex = 0;
+            }
+
+            idx++;
+        }
+
+        PK_TRACE_DBG("INIT: Generated sc=%d", sc);
+    }
 }
