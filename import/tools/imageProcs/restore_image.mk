00771fa993da5aafef4b2cbc41ec2532a0a6b3c7
Prem Shanker Jha <premjha2@in.ibm.com>
2018-04-17 11:24:24 -0500

    Self Restore: Integrated support for build of self-restore code in EKB.
    Self restore binary used to get built by a shell script standalone.
    It used to get checked in to EKB. Hardware image build infrastructure
    used to just pick that binary and insert it into the hardware image.
    This commit augments the EKB build flow by incorporating a capability
    to build self restore binary like any other executable generated
    during EKB build. Syntax to build binary:
        ekb build p9_core_restore_routines
    
     Key_Cronus_Test=PM_REGRESS
    
    Change-Id: Ifdcf31df6ceded564222f6962d94a3459072a3d3
    Reviewed-on: http://ralgit01.raleigh.ibm.com/gerrit1/57345
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Cronus HW CI <cronushw-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Kahn C. Evans <kahnevan@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Richard J. Knight <rjknight@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- import/tools/imageProcs/restore_image.mk
+++ import/tools/imageProcs/restore_image.mk
@@ -37,7 +37,7 @@ $(eval $(call BUILD_DD_LEVEL_CONTAINER,$1,cpmr_header))
 
 # files to be appended to image
 $(eval $(IMAGE)_FILE_CPMR=$$($(IMAGE)_DD_CONT_cpmr_header))
-$(eval $(IMAGE)_FILE_SELF=$(ROOTPATH)/chips/p9/procedures/utils/stopreg/selfRest.bin)
+$(eval $(IMAGE)_FILE_SELF=$(ROOTPATH)/output/images/utils/stopreg/p9_core_restore_routines/p9_core_restore_routines.bin)
 
 # dependencies for appending image sections in sequence:
 # - file to be appended
