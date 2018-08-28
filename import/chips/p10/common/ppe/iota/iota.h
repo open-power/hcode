3ff8956de33b563011cd40f57f209427b9360869
Yue Du <daviddu@us.ibm.com>
2018-08-28 08:35:42 -0500

    P10 Powman: initial common file port and qme image structures
    The following change is not included in this commit as being hack
    
    To build qme: ekb build qme_p10dd1
    need to add/change these lines to ekb build tool files
    
    modified tools/build/ekb.dir/mflags.env.mk
    Using new compiler from a temp test dir(Need to formalize this)
    PPE_TOOL_PATH ?= $(CTEPATH)/tools/ppetools/test
    
    modified tools/build/image.dir/cflags.env.mk
    Change to 42x from 42 for new ppe instructions
    ASFLAGS = -mppe42x
    PPE_CFLAGS += -mcpu=ppe42x
    
    modified tools/build/image.dir/ppe.env.mk
    Base source dir for new qme target
    QME_SRCDIR=$(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    modified tools/build/ekb.dir/dirs.env.mk
    For makefile search
    MAKEFILE_PATH += $(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    Change-Id: I41820e4a7577a7a91a27a68d2c9276ab312178f0
    Original-Change-Id: I552ecbe9fd3581c695e8ccad6399152d160da7d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70521
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

---
+++ import/chips/p10/common/ppe/iota/iota.h
@@ -0,0 +1,223 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/common/ppe/iota/iota.h $                     */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2017,2019                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#ifndef __IOTA_H__
+#define __IOTA_H__
+
+#include "iota_app_cfg.h"
+
+#ifndef __ASSEMBLER__
+
+#include <stdint.h>
+#include "iota_ppe42.h"
+#include "pk.h"
+
+// Default for IOTA_IDLE_TASKS_ENABLE is no idle task support
+#if !defined(IOTA_IDLE_TASKS_ENABLE)
+    #define IOTA_IDLE_TASKS_ENABLE 0
+#endif
+
+/**
+ * Run the main IOTA kernel loop
+ * @note does not return
+ */
+void iota_run()  __attribute__((noreturn));
+
+#define iota_halt() __iota_halt()
+#define iota_dead(code) PK_PANIC((code))
+
+#if IOTA_IDLE_TASKS_ENABLE
+    /**
+    * Set the state of an idle task
+    * @param[in] The state
+    * @param[in[ The task index in the idle task table
+    */
+    void iota_set_idle_task_state(uint32_t state, uint32_t idle_task_idx);
+#endif
+
+/// IOTA --- CONSTANTS
+#if !defined(IOTA_STACK_PATTERN)
+    #define IOTA_STACK_PATTERN  0xfeedfeedfeedfeedull
+#endif
+
+#if !defined(IOTA_64U_ARR_INIT)
+    #define IOTA_64U_ARR_INIT   0x6666666666666666ull
+#endif
+
+#if !defined(IOTA_32U_ARR_INIT)
+    #define IOTA_32U_ARR_INIT   0x66666666ul
+#endif
+
+#if !defined(IOTA_08U_ARR_INIT)
+    #define IOTA_08U_ARR_INIT   0x66u
+#endif
+
+#define IOTA_NO_TASK        ((iotaTaskFuncPtr)(IOTA_32U_ARR_INIT))
+#define IOTA_IDLE_DISABLED  0x00000000
+#define IOTA_IDLE_ENABLED   0x00000001
+
+#if !defined(IOTA_INITIAL_MSR)
+    #define IOTA_INITIAL_MSR (MSR_ME | MSR_IS0 | MSR_IS1 | MSR_IS2 | MSR_IPE)
+#endif
+
+#if !defined(IOTA_DEFAULT_MSR)
+#define IOTA_DEFAULT_MSR    (MSR_UIE | MSR_ME | MSR_IS0 | \
+                             MSR_IS1 | MSR_IS2 | MSR_IPE)
+#endif
+
+///
+
+/// IOTA -- CONVENIENCE MACROS
+// This below macro-fu is an adaptation of a similar macro in the Linux kernel
+// (BUILD_BUG_ON). Allows compile time checking of conditions using sizeof()
+#define IOTA_TOKEN_PASTE0(a,b) a ## b
+#define IOTA_TOKEN_PASTE1(a,b) IOTA_TOKEN_PASTE0(a,b)
+#define IOTA_COMPILE_TIME_UNIQUE \
+    IOTA_TOKEN_PASTE1(_iota_compile_unique_,__COUNTER__)
+#define IOTA_COMPILE_TIME_CHECK(condition) \
+    extern int IOTA_COMPILE_TIME_UNIQUE [!!(condition)-1];
+
+// IOTA Plugin functions
+#define IOTA_TASK(function) ((iotaTaskFuncPtr)function)
+#define IOTA_TIMER_HANDLER(function) ((iotaTimerFuncPtr)function)
+
+#define IOTA_DEC_HANDLER(function) g_iota_dec_handler \
+        = IOTA_TIMER_HANDLER(function);
+
+#define IOTA_FIT_HANDLER(function) g_iota_fit_handler \
+        = IOTA_TIMER_HANDLER(function);
+
+#if IOTA_IDLE_TASKS_ENABLE
+
+#define IOTA_BEGIN_IDLE_TASK_TABLE \
+    iotaIdleTask g_iota_idle_task_list[] \
+    SECTION(".sdata.g_iota_idle_task_list") = {
+#define IOTA_END_IDLE_TASK_TABLE \
+    }; \
+    uint32_t const g_iota_idle_task_list_size \
+        = (uint32_t)(sizeof(g_iota_idle_task_list)/sizeof(iotaIdleTask));
+#endif
+
+#define IOTA_BEGIN_TASK_TABLE \
+    iotaTaskFuncPtr g_iota_task_list[] SECTION(".sdata.g_iota_task_list") = {
+#define IOTA_END_TASK_TABLE \
+    }; \
+    uint32_t const g_iota_task_list_size \
+        = (uint32_t)(sizeof(g_iota_task_list)/sizeof(iotaTaskFuncPtr)); \
+    IOTA_COMPILE_TIME_CHECK((sizeof(g_iota_task_list)/sizeof(iotaTaskFuncPtr)) \
+                            == (IOTA_NUM_EXT_IRQ_PRIORITIES));
+
+#define IOTA_MACHINE_STATE_INIT { \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT, \
+        IOTA_32U_ARR_INIT \
+    }
+///
+
+typedef struct
+{
+    uint32_t back_chain;
+    uint32_t next_lr;
+    uint32_t CR;
+    uint32_t SPRG0;
+    uint32_t GPR0;
+    uint32_t GPR1;
+    uint32_t GPR3;
+    uint32_t GPR4;
+    uint32_t GPR5;
+    uint32_t GPR6;
+    uint32_t GPR7;
+    uint32_t GPR8;
+    uint32_t GPR9;
+    uint32_t GPR10;
+    uint32_t XER;
+    uint32_t CTR;
+    uint32_t SRR0;
+    uint32_t SRR1;
+    uint32_t GPR28;
+    uint32_t GPR29;
+    uint32_t GPR30;
+    uint32_t GPR31;
+} iotaMachineState;
+
+typedef void (*iotaTaskFuncPtr )( );
+typedef void (*iotaTimerFuncPtr)(void);
+
+typedef struct
+{
+    uint32_t        state;
+    iotaTaskFuncPtr function;
+} iotaIdleTask;
+
+#define MACHINE_STATE_CONTEXT_SIZE  sizeof(iotaMachineState)
+#define MACHINE_STATE_STACK_SIZE (IOTA_MAX_NESTED_INTERRUPTS * MACHINE_STATE_CONTEXT_SIZE)
+#define IOTA_STACK_DWORD_SIZE ((IOTA_EXECUTION_STACK_SIZE + MACHINE_STATE_STACK_SIZE)/8 + 1)
+
+// IF IOTA_IDLE_TASKS_ENABLE,
+// This variable just becomes an interrupt level count to
+// determine when idle tasks can run.
+extern uint32_t           g_iota_curr_machine_state_ptr;
+extern iotaTaskFuncPtr    g_iota_task_list[];
+extern uint32_t const     g_iota_task_list_size;
+extern uint64_t           g_iota_execution_stack[];
+extern uint64_t           g_iota_execution_stack_end;
+
+#if IOTA_IDLE_TASKS_ENABLE
+    extern iotaIdleTask       g_iota_idle_task_list[];
+    extern uint32_t const     g_iota_idle_task_list_size;
+#endif
+
+void _iota_boot();
+void _iota_schedule(uint32_t schedule_reason);
+void _iota_evaluate_idle_tasks();
+
+extern void __iota_halt() __attribute__((noreturn));
+
+extern iotaTimerFuncPtr g_iota_dec_handler;
+extern iotaTimerFuncPtr g_iota_fit_handler;
+
+extern uint32_t __ext_irq_handler(void);
+extern void     __ext_irq_resume(void);
+extern void     __hwmacro_setup(void);
+
+#endif // __ASSEMBLER__
+
+#endif //__IOTA_H__
---
+++ import/chips/p10/common/ppe/iota/iota_ppe42.h
@@ -0,0 +1,255 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/common/ppe/iota/iota_ppe42.h $               */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2017,2019                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#ifndef __ASSEMBLER__
+    #include <stdint.h>
+#endif
+
+#include "ppe42_asm.h"
+#include "ppe42_msr.h"
+#include "ppe42_spr.h"
+
+#ifndef __IOTA_PPE42_H__
+#define __IOTA_PPE42_H__
+
+#ifdef __PPE42_CORE_C__
+    #define IF__PPE42_CORE_C__(x) x
+    #define UNLESS__PPE42_CORE_C__(x)
+#else
+    #define IF__PPE42_CORE_C__(x)
+    #define UNLESS__PPE42_CORE_C__(x) x
+#endif
+
+#define _IOTA_SCHEDULE_REASON_FIT 0x0f
+#define _IOTA_SCHEDULE_REASON_DEC 0x0d
+#define _IOTA_SCHEDULE_REASON_EXT 0x0e
+
+#ifndef PPE42_DBCR_INITIAL
+    // Enable 'trap' to cause a halt
+    #define PPE42_DBCR_INITIAL 0x01000000
+#endif
+
+#define mtdbcr(value) \
+    asm volatile ("mtdbcr %0" : : "r" (value) : "memory")
+
+#define mttcr(value) \
+    asm volatile ("mttcr %0" : : "r" (value) : "memory")
+
+#define sync() asm volatile ("sync" : : : "memory")
+
+#ifdef __ASSEMBLER__
+// *INDENT-OFF*
+
+# halt ppe with panic code macro
+.macro _pk_panic, code
+    tw  31,(\code)/256, (\code)%256
+.endm
+
+# EABI set SDATA2(r2) ,SDATA(r13), and stack pointer
+# Call to _iota_boot should never return
+# 7 instructions MAX!!
+.macro __m_iota_boot
+    lis     %r13, _SDA_BASE_@h
+    ori     %r13, %r13, _SDA_BASE_@l
+    lis     %r2, _SDA2_BASE_@h
+    ori     %r2, %r2, _SDA2_BASE_@l
+    lis     %r1, g_iota_execution_stack_end@h
+    ori     %r1, %r1, g_iota_execution_stack_end@l
+    bl      _iota_boot
+.endm
+
+
+#ifdef __PPE42X__
+# This routine is called on any exception or interrupt and saves some state
+# prior to setting up the call to and calling __iota_save_interrupt_state_and_schedule
+# Note: must be <=8 instructions!
+.macro __m_iota_interrupt_and_exception_handler, iota_schedule_reason
+# Creates Stack frame and saves CR,SPRG0,R0,R1,R3-R10,XER,CTR,SRR0,SRR1,R28-R31
+stcxtu  %r1,-88(%r1)
+li      %r3, \iota_schedule_reason
+bl      _iota_schedule
+lcxt    %r1,%r1
+rfi
+.endm
+#endif
+
+// *INDENT-ON*
+#else
+
+/// 8-bit MMIO Write
+#define out8(addr, data) \
+    do {*(volatile uint8_t *)(addr) = (data);} while(0)
+
+/// 8-bit MMIO Read
+#define in8(addr) \
+    ({uint8_t __data = *(volatile uint8_t *)(addr); __data;})
+
+/// 16-bit MMIO Write
+#define out16(addr, data) \
+    do {*(volatile uint16_t *)(addr) = (data);} while(0)
+
+/// 16-bit MMIO Read
+#define in16(addr) \
+    ({uint16_t __data = *(volatile uint16_t *)(addr); __data;})
+
+/// 32-bit MMIO Write
+#define out32(addr, data) \
+    do {*(volatile uint32_t *)(addr) = (data);} while(0)
+
+/// 32-bit MMIO Read
+#define in32(addr) \
+    ({uint32_t __data = *(volatile uint32_t *)(addr); __data;})
+
+/// 64-bit MMIO Write
+#define out64(addr, data) \
+    {\
+        uint64_t __d = (data); \
+        uint32_t* __a = (uint32_t*)(addr); \
+        asm volatile \
+        (\
+         "stvd %1, %0 \n" \
+         : "=o"(*__a) \
+         : "r"(__d) \
+        ); \
+    }
+
+/// 64-bit MMIO Read
+#define in64(addr) \
+    ({\
+        uint64_t __d; \
+        uint32_t* __a = (uint32_t*)(addr); \
+        asm volatile \
+        (\
+         "lvd %0, %1 \n" \
+         :"=r"(__d) \
+         :"o"(*__a) \
+        ); \
+        __d; \
+    })
+
+/// CouNT Leading Zeros Word
+#define cntlzw(x) \
+    ({uint32_t __x = (x); \
+        uint32_t __lzw; \
+        asm volatile ("cntlzw %0, %1" : "=r" (__lzw) : "r" (__x)); \
+        __lzw;})
+
+/// CouNT Leading Zeros : uint32_t
+static inline int
+cntlz32(uint32_t x)
+{
+    return cntlzw(x);
+}
+
+/// CouNT Leading Zeros : uint64_t
+static inline int
+cntlz64(uint64_t x)
+{
+    if (x > 0xffffffff)
+    {
+        return cntlz32(x >> 32);
+    }
+    else
+    {
+        return 32 + cntlz32(x);
+    }
+}
+
+
+// machine context is simply the MSR
+typedef unsigned long PkMachineContext;
+
+/// Enter a critical section, saving the current machine
+/// context.
+UNLESS__PPE42_CORE_C__(extern)
+inline
+void pk_interrupt_disable(PkMachineContext* context)
+{
+    *context = mfmsr();
+    wrteei(0);
+}
+
+UNLESS__PPE42_CORE_C__(extern)
+inline
+void pk_machine_context_set(PkMachineContext* context)
+{
+    mtmsr(*context);
+}
+
+#define pk_critical_section_enter(pctx) \
+    pk_interrupt_disable(pctx)
+
+/// Exit a critical section by restoring the previous machine context.
+
+#define pk_critical_section_exit(pctx) \
+    pk_machine_context_set(pctx)
+
+typedef union
+{
+
+    uint32_t value;
+
+    struct
+    {
+
+        /// A flag indicating that PK is in thread mode after a call of
+        /// pk_start_threads().
+        unsigned thread_mode : 1;
+
+        /// If this field is non-zero then PK is processing an interrupt
+        /// and the \c irq field will contain the PkIrqId of the interrupt
+        /// that kicked off interrupt processing.
+        unsigned processing_interrupt : 1;
+
+        /// The priority of the currently running thread.  In an interrupt
+        /// context, this is the priority of the thread that was interrupted.
+        unsigned thread_priority : 6;
+
+        /// This bit tracks whether the current context can be discarded or
+        /// if the context must be saved.  If the processor takes an interrupt
+        /// and this bit is set, then the current context will be discarded.
+        /// This bit is set at the end of handling an interrupt and prior
+        /// to entering the wait enabled state.
+        unsigned discard_ctx : 1;
+
+        /// The PkIrqId of the currently running (or last run) handler.  If
+        /// \c processing_interrupt is set, then this is the
+        /// PkIrqId of the IRQ that is currently executing.
+        unsigned irq : 7;
+
+        /// Each PPE application will define (or not) the interpretation of
+        /// this field.  Since SPRG0 is saved and restored during during thread
+        /// context switches, this field can be used to record the progress of
+        /// individual threads.  The kernel and/or application will provide
+        /// APIs or macros to read and write this field.
+        unsigned app_specific : 16;
+
+    } fields;
+
+} __KernelContext;
+
+#endif // __ASSEMBLER__
+
+#endif // __IOTA_PPE42_H__
