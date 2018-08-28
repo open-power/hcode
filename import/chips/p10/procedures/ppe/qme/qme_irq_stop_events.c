2307be63865a6a307aabbf5b11a4d92fe114183d
Yue Du <daviddu@us.ibm.com>
2018-08-28 08:35:42 -0500

    P10 Powman: initial common file port and qme image structures
    The following change is not included in this commit as being hack
    
    To build qme: ekb build qme_p10dd1
    need to add/change these lines to ekb build tool files
    
    modified tools/build/ekb.dir/mflags.env.mk
    Using new compiler from a temp test dir(Need to formalize this)
    PPE_TOOL_PATH ?= $(CTEPATH)/tools/ppetools/test
    
    modified tools/build/image.dir/cflags.env.mk
    Change to 42x from 42 for new ppe instructions
    ASFLAGS = -mppe42x
    PPE_CFLAGS += -mcpu=ppe42x
    
    modified tools/build/image.dir/ppe.env.mk
    Base source dir for new qme target
    QME_SRCDIR=$(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    modified tools/build/ekb.dir/dirs.env.mk
    For makefile search
    MAKEFILE_PATH += $(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    Change-Id: I2746ebda848caf6fe46b2d3e3d587596c211e5df
    Original-Change-Id: I552ecbe9fd3581c695e8ccad6399152d160da7d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70521
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/qme/qme_irq_stop_events.c
@@ -0,0 +1,108 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/ppe/qme/qme_irq_stop_events.c $   */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2018,2020                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#include "qme.h"
+
+extern QmeRecord G_qme_record;
+
+/*
+void
+qme_eval_eimr_override()
+{
+    wrteei(0);
+
+    g_eimr_override      &= ~BITS64(12, 10);
+    data64_t mask_irqs    = {0};
+    mask_irqs.words.lower = 0;
+
+    mask_irqs.words.upper =
+        ((((~G_cme_record.core_enabled)   |
+           G_qme_record.core_running |
+           G_qme_record.core_errored |
+           G_qme_record.core_blockpc |
+           G_qme_record.core_blockwu |
+           G_qme_record.core_suspendwu) & CME_MASK_BC) << SHIFT32(13)) |
+#if SPWU_AUTO
+        ((((~G_cme_record.core_enabled)   |
+           G_qme_record.core_running |
+           G_qme_record.core_errored) & CME_MASK_BC) << SHIFT32(15)) |
+#else
+        ((((~G_qme_record.core_running) &
+           (G_qme_record.core_blockwu |
+            G_qme_record.core_suspendwu)) & CME_MASK_BC) << SHIFT32(15)) |
+#endif
+        ((((~G_cme_record.core_enabled)   |
+           G_qme_record.core_running |
+           G_qme_record.core_errored |
+           G_qme_record.core_blockwu |
+           G_qme_record.core_suspendwu) & CME_MASK_BC) << SHIFT32(17)) |
+        ((((~G_cme_record.core_enabled)      |
+           (~G_qme_record.core_running) |
+           G_qme_record.core_errored    |
+           G_qme_record.core_in_spwu    |
+           G_qme_record.core_blockey    |
+           G_qme_record.core_vdm_droop  |
+           G_qme_record.core_suspendey) & CME_MASK_BC) << SHIFT32(21));
+
+    g_eimr_override |= mask_irqs.value;
+
+    if (G_qme_record.core_vdm_droop)
+    {
+        out32(G_CME_LCL_FLAGS_OR, BIT32(CME_FLAGS_DROOP_SUSPEND_ENTRY));
+    }
+    else
+    {
+        out32(G_CME_LCL_FLAGS_CLR, BIT32(CME_FLAGS_DROOP_SUSPEND_ENTRY));
+    }
+
+    wrteei(1);
+}
+*/
+
+void
+qme_special_wakeup_event()
+{
+    PK_TRACE("Event: Special Wakeup");
+
+}
+
+void
+qme_regular_wakeup_event()
+{
+    PK_TRACE("Event: Regular Wakeup");
+}
+
+void
+qme_pm_state_active_event()
+{
+    PK_TRACE("Event: PM State Active");
+}
+
+
+void
+qme_mma_active_event(uint32_t task_idx)
+{
+    PK_TRACE("Event: MMA Active");
+}
