0ff682f640dfc3af67a01614810825144e798f0d
Yue Du <daviddu@us.ibm.com>
2018-08-28 08:35:42 -0500

    P10 Powman: initial common file port and qme image structures
    The following change is not included in this commit as being hack
    
    To build qme: ekb build qme_p10dd1
    need to add/change these lines to ekb build tool files
    
    modified tools/build/ekb.dir/mflags.env.mk
    Using new compiler from a temp test dir(Need to formalize this)
    PPE_TOOL_PATH ?= $(CTEPATH)/tools/ppetools/test
    
    modified tools/build/image.dir/cflags.env.mk
    Change to 42x from 42 for new ppe instructions
    ASFLAGS = -mppe42x
    PPE_CFLAGS += -mcpu=ppe42x
    
    modified tools/build/image.dir/ppe.env.mk
    Base source dir for new qme target
    QME_SRCDIR=$(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    modified tools/build/ekb.dir/dirs.env.mk
    For makefile search
    MAKEFILE_PATH += $(ROOTPATH)/chips/p10/procedures/ppe/qme
    
    Change-Id: Ic0ec7e81bc072dce1331a1ba85b60fa1df57ab51
    Original-Change-Id: I552ecbe9fd3581c695e8ccad6399152d160da7d7
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70521
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/qme/qme_edit.mk
@@ -0,0 +1,39 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p10/procedures/ppe/qme/qme_edit.mk $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2016,2025
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+EXE=cmeImgEdit
+
+$(EXE)_COMMONFLAGS+= -D__PPE_PLAT
+
+$(call ADD_EXE_INCDIR, $(EXE), \
+   $(CME_SRCDIR) \
+   $(PK_SRCDIR)/kernel \
+   $(HCODE_COMMON_LIBDIR) \
+   $(HCODE_LIBDIR) \
+    )
+
+
+IMAGE_DEPS+=cmeImgEdit
+OBJS=p9_cme_img_edit.o
+$(call BUILD_EXE)
