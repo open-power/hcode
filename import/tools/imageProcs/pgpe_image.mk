6fc1020ce0f345daac641ec2ac97c3a61d18d9f7
Claus Michael Olsen <cmolsen@us.ibm.com>
2018-09-23 08:21:53 -0500

    P10 prep: IPL image [formerly XIP image] changes (1)
    Summary of changes:
    - Removed support for PPE ringSection coordination within HW image's
      .rings section in the TOR API.
    - Removed the .rings IPL image section from the HW image.
    - Added the nested SBE image to HW image. So now, for consistency, all
      PPEs that use rings, namely SBE, CME and SGPE, have nested images within
      the HW image.
    - Added .rings sections to all nested PPE images (since PPE ringSection
      packaging is no longer in TOR and thus no longer in HW image's .rings
      ringSection).
    - Renamed pstate_gpe* to pgpe*.
    - Updated ring_apply to produce individual PPE ring section files and to no
      longer produce a HW .rings ringSection file.
    - Updated TOR API to no longer accommodate PPE ringSections within HW
      image's .rings ringSection. This also enabled the elimation of
      tor_get_block_of_rings.
    - Introduced p9_xip_get_sub_section() to the ipl_image API to make it
      easier for user codes to retrieve PPE ring sections which is now located
      in the nested PPE images.
    - Updated ipl_image_tool (formerly xip_tool) to no longer have to consider
      looping over PPE ring sections (since now they are split up into
      their respective PPE image's .rings sections).
    - Updated xip_customize to use the new nested image API to retrieve the
      ringSection from the nested PPE (CME and SGPE) images.
    - Updated ipl_build to use the new nested image API to retrieve the
      ringSection from the nested SBE image.
    - Updated several Cronus wrappers to use the modified ipl_image and TOR
      APIs. Also, removed one of the Cronus wrappers.
    
    Key_Cronus_Test=XIP_REGRESS
    
    Change-Id: I3519e6e50af5e401903f0e925bdd2a4c58d30448
    Original-Change-Id: I51cbcec0c6362c5f53a41cae2961fbb29bd8cf17
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/70117
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ import/tools/imageProcs/pgpe_image.mk
@@ -0,0 +1,89 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/tools/imageProcs/pgpe_image.mk $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2016,2019
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+# $1 == chipId
+define BUILD_PGPE_IMAGE
+$(eval IMAGE=$1.pgpe_image)
+
+$(eval $(IMAGE)_PATH=$(IMAGEPATH)/pgpe_image)
+$(eval $(IMAGE)_LINK_SCRIPT=pgpe_image.cmd)
+$(eval $(IMAGE)_LAYOUT=$(IMAGEPATH)/pgpe_image/pgpe_image.o)
+$(eval pgpe_image_COMMONFLAGS += -I$(ROOTPATH)/chips/p10/utils/imageProcs/)
+
+# files with multiple DD level content to be generated
+$(eval $(call BUILD_DD_LEVEL_CONTAINER,$1,ppmr_header))
+$(eval $(call BUILD_DD_LEVEL_CONTAINER,$1,pstate_gpe))
+
+# files to be appended to image
+$(eval $(IMAGE)_FILE_AUX_TASK=$(IMAGEPATH)/p9_pgpe_aux_task/p9_pgpe_aux_task.bin)
+$(eval $(IMAGE)_FILE_PPMR_HDR=$$($(IMAGE)_DD_CONT_ppmr_header))
+$(eval $(IMAGE)_FILE_LVL1_BL=$(IMAGEPATH)/pgpe_lvl1_copier/pgpe_lvl1_copier.bin)
+$(eval $(IMAGE)_FILE_LVL2_BL=$(IMAGEPATH)/pgpe_lvl2_loader/pgpe_lvl2_loader.bin)
+$(eval $(IMAGE)_FILE_HCODE=$$($(IMAGE)_DD_CONT_pstate_gpe))
+
+# dependencies for appending image sections in sequence:
+# - file to be appended
+# - all dependencies of previously appended sections or on raw image
+# - append operation as to other section that has to be finished first
+$(eval $(IMAGE)_DEPS_AUX_TASK =$$($(IMAGE)_FILE_AUX_TASK))
+$(eval $(IMAGE)_DEPS_AUX_TASK+=$$($(IMAGE)_PATH)/.$(IMAGE).setbuild_host)
+
+$(eval $(IMAGE)_DEPS_PPMR_HDR =$$($(IMAGE)_FILE_PPMR_HDR))
+$(eval $(IMAGE)_DEPS_PPMR_HDR+=$$($(IMAGE)_DEPS_AUX_TASK))
+$(eval $(IMAGE)_DEPS_PPMR_HDR+=$$($(IMAGE)_PATH)/.$(IMAGE).append.aux_task)
+
+$(eval $(IMAGE)_DEPS_LVL1_BL =$$($(IMAGE)_FILE_LVL1_BL))
+$(eval $(IMAGE)_DEPS_LVL1_BL+=$$($(IMAGE)_DEPS_PPMR_HDR))
+$(eval $(IMAGE)_DEPS_LVL1_BL+=$$($(IMAGE)_PATH)/.$(IMAGE).append.ppmr_header)
+
+$(eval $(IMAGE)_DEPS_LVL2_BL =$$($(IMAGE)_FILE_LVL2_BL))
+$(eval $(IMAGE)_DEPS_LVL2_BL+=$$($(IMAGE)_DEPS_LVL1_BL))
+$(eval $(IMAGE)_DEPS_LVL2_BL+=$$($(IMAGE)_PATH)/.$(IMAGE).append.lvl1_bl)
+
+$(eval $(IMAGE)_DEPS_HCODE =$$($(IMAGE)_FILE_HCODE))
+$(eval $(IMAGE)_DEPS_HCODE+=$$($(IMAGE)_DEPS_LVL2_BL))
+$(eval $(IMAGE)_DEPS_HCODE+=$$($(IMAGE)_PATH)/.$(IMAGE).append.lvl2_bl)
+
+$(eval $(IMAGE)_DEPS_REPORT =$$($(IMAGE)_DEPS_HCODE))
+$(eval $(IMAGE)_DEPS_REPORT+=$$($(IMAGE)_PATH)/.$(IMAGE).append.hcode)
+
+# image build using all files and serialised by dependencies
+$(eval $(call XIP_TOOL,append,.aux_task,$$($(IMAGE)_DEPS_AUX_TASK),$$($(IMAGE)_FILE_AUX_TASK)))
+$(eval $(call XIP_TOOL,append,.ppmr_header,$$($(IMAGE)_DEPS_PPMR_HDR),$$($(IMAGE)_FILE_PPMR_HDR) 1))
+$(eval $(call XIP_TOOL,append,.lvl1_bl,$$($(IMAGE)_DEPS_LVL1_BL),$$($(IMAGE)_FILE_LVL1_BL)))
+$(eval $(call XIP_TOOL,append,.lvl2_bl,$$($(IMAGE)_DEPS_LVL2_BL),$$($(IMAGE)_FILE_LVL2_BL)))
+$(eval $(call XIP_TOOL,append,.hcode,$$($(IMAGE)_DEPS_HCODE),$$($(IMAGE)_FILE_HCODE) 1))
+
+# create image report for image with all files appended
+$(eval $(call XIP_TOOL,report,,$$($(IMAGE)_DEPS_REPORT)))
+
+$(eval $(call BUILD_XIPIMAGE))
+endef
+
+$(eval CHIPS := $(filter-out ocmb,$(CHIPS)))
+
+$(foreach chip,$(CHIPS),\
+	$(foreach chipId, $($(chip)_CHIPID),\
+		$(eval $(call BUILD_PGPE_IMAGE,$(chipId)))))
