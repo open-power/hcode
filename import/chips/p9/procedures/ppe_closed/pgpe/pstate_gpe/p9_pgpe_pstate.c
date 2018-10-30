a13f21bb82649455360cf49bf17e28874e5f4098
Rahul Batra <rbatra@us.ibm.com>
2018-10-29 19:06:10 -0500

    PGPE: Only ack pending Quad Exit on WOF Disable
        Key_Cronus_Test=PM_REGRESS
    
    Change-Id: I5da32db386d84f14f39a37e049fc4730720d6974
    CQ: SW449940
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/68389
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- import/chips/p9/procedures/ppe_closed/pgpe/pstate_gpe/p9_pgpe_pstate.c
+++ import/chips/p9/procedures/ppe_closed/pgpe/pstate_gpe/p9_pgpe_pstate.c
@@ -1200,7 +1200,8 @@ void p9_pgpe_pstate_wof_ctrl(uint32_t action)
 
         //ACK any pending quad active update exit. PGPE might have been waiting for WOF VFRT from OCC, but
         //with WOF disabled that will never come.
-        if (G_pgpe_pstate_record.ipcPendTbl[IPC_PEND_SGPE_ACTIVE_QUADS_UPDT].pending_ack == 1)
+        if (G_pgpe_pstate_record.ipcPendTbl[IPC_PEND_SGPE_ACTIVE_QUADS_UPDT].pending_processing == 0 &&
+            G_pgpe_pstate_record.ipcPendTbl[IPC_PEND_SGPE_ACTIVE_QUADS_UPDT].pending_ack == 1)
         {
             ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)G_pgpe_pstate_record.ipcPendTbl[IPC_PEND_SGPE_ACTIVE_QUADS_UPDT].cmd;
             ipcmsg_s2p_update_active_quads_t* args = (ipcmsg_s2p_update_active_quads_t*)async_cmd->cmd_data;
