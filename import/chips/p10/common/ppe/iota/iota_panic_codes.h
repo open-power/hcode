81416ce76ed9876afc9d4f1cab7b64dba40c5867
Douglas Gilbert <dgilbert@us.ibm.com>
2019-01-30 16:37:48 -0600

    hcode: GPE IPC support for IOTA
    Change-Id: Ie0f31adc35d38109676f21019ee9d402672451ab
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/71140
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RAHUL BATRA <rbatra@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- import/chips/p10/common/ppe/iota/iota_panic_codes.h
+++ import/chips/p10/common/ppe/iota/iota_panic_codes.h
@@ -137,10 +137,10 @@ typedef enum
     IOTA_UNUSED_0504 = 0x0504,
     IOTA_UNUSED_0505 = 0x0505,
     IOTA_UNUSED_0506 = 0x0506,
-    IOTA_UNUSED_0507 = 0x0507,
-    IOTA_UNUSED_0508 = 0x0508,
-    IOTA_UNUSED_0509 = 0x0509,
-    IOTA_UNUSED_050a = 0x050a,
+    OCCHW_INSTANCE_MISMATCH             = 0x0507,
+    OCCHW_IRQ_ROUTING_ERROR             = 0x0508,
+    OCCHW_XIR_INVALID_POINTER           = 0x0509,
+    OCCHW_XIR_INVALID_GPE               = 0x050a,
     IOTA_UNUSED_050d = 0x050d,
     IOTA_UNUSED_051c = 0x051c,
     IOTA_UNUSED_051d = 0x051d,
@@ -253,7 +253,84 @@ typedef enum
     IOTA_UNUSED_0d1c = 0x0d1c,
     IOTA_UNUSED_0d1d = 0x0d1d,
     IOTA_UNUSED_0d1e = 0x0d1e,
-    IOTA_UNUSED_0d1f = 0x0d1f
+    IOTA_UNUSED_0d1f = 0x0d1f,
+
+    // The following are reserved for instance specific use.
+    // Each engine must define its own XXX_panic_codes.h
+    // Where XXX = SBE, QME, GPE0, GPE1, PGPE, XGPE
+    // They are listed here to show the valid trap values that
+    // can be used.
+
+#ifdef PLATFORM_PANIC_CODES_H
+#include PLATFORM_PANIC_CODES_H
+#endif
+    //_UNUSED_1c00                      = 0x1c00,
+    //_UNUSED_1c01                      = 0x1c01,
+    //_UNUSED_1c02                      = 0x1c02,
+    //_UNUSED_1c03                      = 0x1c03,
+    //_UNUSED_1c04                      = 0x1c04,
+    //_UNUSED_1c05                      = 0x1c05,
+    //_UNUSED_1c06                      = 0x1c06,
+    //_UNUSED_1c07                      = 0x1c07,
+    //_UNUSED_1c08                      = 0x1c08,
+    //_UNUSED_1c09                      = 0x1c09,
+    //_UNUSED_1c0a                      = 0x1c0a,
+    //_UNUSED_1c0d                      = 0x1c0d,
+    //_UNUSED_1c1c                      = 0x1c1c,
+    //_UNUSED_1c1d                      = 0x1c1d,
+    //_UNUSED_1c1e                      = 0x1c1e,
+    //_UNUSED_1c1f                      = 0x1c1f,
+
+    //_UNUSED_1d00                      = 0x1d00,
+    //_UNUSED_1d01                      = 0x1d01,
+    //_UNUSED_1d02                      = 0x1d02,
+    //_UNUSED_1d03                      = 0x1d03,
+    //_UNUSED_1d04                      = 0x1d04,
+    //_UNUSED_1d05                      = 0x1d05,
+    //_UNUSED_1d06                      = 0x1d06,
+    //_UNUSED_1d07                      = 0x1d07,
+    //_UNUSED_1d08                      = 0x1d08,
+    //_UNUSED_1d09                      = 0x1d09,
+    //_UNUSED_1d0a                      = 0x1d0a,
+    //_UNUSED_1d0d                      = 0x1d0d,
+    //_UNUSED_1d1c                      = 0x1d1c,
+    //_UNUSED_1d1d                      = 0x1d1d,
+    //_UNUSED_1d1e                      = 0x1d1e,
+    //_UNUSED_1d1f                      = 0x1d1f,
+
+    //_UNUSED_1e00                      = 0x1e00,
+    //_UNUSED_1e01                      = 0x1e01,
+    //_UNUSED_1e02                      = 0x1e02,
+    //_UNUSED_1e03                      = 0x1e03,
+    //_UNUSED_1e04                      = 0x1e04,
+    //_UNUSED_1e05                      = 0x1e05,
+    //_UNUSED_1e06                      = 0x1e06,
+    //_UNUSED_1e07                      = 0x1e07,
+    //_UNUSED_1e08                      = 0x1e08,
+    //_UNUSED_1e09                      = 0x1e09,
+    //_UNUSED_1e0a                      = 0x1e0a,
+    //_UNUSED_1e0d                      = 0x1e0d,
+    //_UNUSED_1e1c                      = 0x1e1c,
+    //_UNUSED_1e1d                      = 0x1e1d,
+    //_UNUSED_1e1e                      = 0x1e1e,
+    //_UNUSED_1e1f                      = 0x1e1f,
+
+    //_UNUSED_1f00                      = 0x1f00,
+    //_UNUSED_1f01                      = 0x1f01,
+    //_UNUSED_1f02                      = 0x1f02,
+    //_UNUSED_1f03                      = 0x1f03,
+    //_UNUSED_1f04                      = 0x1f04,
+    //_UNUSED_1f05                      = 0x1f05,
+    //_UNUSED_1f06                      = 0x1f06,
+    //_UNUSED_1f07                      = 0x1f07,
+    //_UNUSED_1f08                      = 0x1f08,
+    //_UNUSED_1f09                      = 0x1f09,
+    //_UNUSED_1f0a                      = 0x1f0a,
+    //_UNUSED_1f0d                      = 0x1f0d,
+    //_UNUSED_1f1c                      = 0x1f1c,
+    //_UNUSED_1f1d                      = 0x1f1d,
+    //_UNUSED_1f1e                      = 0x1f1e,
+    //_UNUSED_1f1f                      = 0x1f1f
 } iotaPanicCode_t;
 
 #else
@@ -269,4 +346,23 @@ typedef enum
 #define IOTA_WATCHDOG_NOT_SUPPORTED        0x000d
 
 #endif  // __ASSEMBLER__
+
+#if !defined(IOTA_PANIC)
+#if SIMICS_ENVIRONMENT
+#define IOTA_PANIC(code)                  \
+    do {                                \
+        asm volatile ("stw     %r3, __pk_panic_save_r3@sda21(0)");      \
+        asm volatile ("lwz     %r3, __pk_panic_dbcr@sda21(0)");         \
+        asm volatile ("mtdbcr  %r3");                                   \
+        asm volatile (".long %0" : : "i" (code));                       \
+    } while(0)
+#else
+#define IOTA_PANIC(code)                                                  \
+    do {                                                                \
+        asm volatile ("tw 31, %0, %1" : : "i" (code/256) , "i" (code%256)); \
+    } while (0)
+#endif // SIMICS_ENVIRONMENT
+#endif // IOTA_PANIC
+
+
 #endif
