b413a6214a3d9f464c0c57c4d23438f3e6a551b4
Rahul Batra <rbatra@us.ibm.com>
2019-03-29 13:09:50 -0400

    PGPE: New P10 Infrastructure
    Change-Id: Ieebb7725a657e253ab37956a77095c8995506e53
    Original-Change-Id: I2738de2f218e2720415d0314389af480c38a7761
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75237
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_event_process.h
@@ -0,0 +1,44 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/ppe/pgpe/pgpe_event_process.h $   */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2019,2020                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+#ifndef __PGPE_PROCESS_EVENT_H__
+#define __PGPE_PROCESS_EVENT_H__
+
+
+void pgpe_process_pstate_start_stop(void* args);
+void pgpe_process_clip_update(void* args);
+void pgpe_process_clip_update_w_ack(void* args);
+void pgpe_process_pmcr_request(void* args);
+void pgpe_process_wof_enable(void* args);
+void pgpe_process_wof_disable(void* args);
+void pgpe_process_wof_vfrt(void* args);
+void pgpe_process_safe_mode(void* args);
+void pgpe_process_complex_suspend(void* args);
+void pgpe_process_occ_fault();
+void pgpe_process_qme_fault();
+void pgpe_process_xgpe_fault();
+void pgpe_process_pvref_fault();
+
+
+#endif
