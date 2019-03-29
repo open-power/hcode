a5498f425419ca37beaf9052edee209a97179186
Rahul Batra <rbatra@us.ibm.com>
2019-03-29 13:09:50 -0400

    PGPE: New P10 Infrastructure
    Change-Id: I2738de2f218e2720415d0314389af480c38a7761
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75237
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_resclk.c
@@ -0,0 +1,38 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/ppe/pgpe/pgpe_resclk.c $          */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2019                                                         */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#include "pgpe_resclk.h"
+
+void pgpe_resclk_init()
+{
+}
+
+void pgpe_resclk_update_pstate_target()
+{
+}
+
+void pgpe_resclk_poll_done()
+{
+}
