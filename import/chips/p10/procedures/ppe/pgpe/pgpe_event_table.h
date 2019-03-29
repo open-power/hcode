773d1a15498d3653efc5fc27a2f6120763bf08de
Rahul Batra <rbatra@us.ibm.com>
2019-03-29 13:09:50 -0400

    PGPE: New P10 Infrastructure
    Change-Id: I63ec6b28d95b6d3e2436a73c08b08b76e40f6636
    Original-Change-Id: I2738de2f218e2720415d0314389af480c38a7761
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75237
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Douglas R. Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_event_table.h
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_event_table.h
@@ -1,11 +1,11 @@
 /* IBM_PROLOG_BEGIN_TAG                                                   */
 /* This is an automatically generated prolog.                             */
 /*                                                                        */
-/* $Source: import/chips/p10/procedures/ppe/pgpe/pgpe_irq_handlers.h $    */
+/* $Source: import/chips/p10/procedures/ppe/pgpe/pgpe_event_table.h $     */
 /*                                                                        */
 /* OpenPOWER EKB Project                                                  */
 /*                                                                        */
-/* COPYRIGHT 2018,2021                                                    */
+/* COPYRIGHT 2019,2021                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -22,24 +22,52 @@
 /* permissions and limitations under the License.                         */
 /*                                                                        */
 /* IBM_PROLOG_END_TAG                                                     */
-#ifndef __PGPE_IRQ_HANDLERS__
-#define __PGPE_IRQ_HANDLERS__
+#ifndef __PGPE_EVENT_TABLE_H__
+#define __PGPE_EVENT_TABLE_H__
 
 #include "pgpe.h"
 
-void pgpe_irq_init();
-void pgpe_irq_fit_handler();
+enum EVENT_TABLE_IDX
+{
+    EV_IPC_PSTATE_START_STOP            =   0,
+    EV_IPC_CLIP_UPDT                    =   1,
+    EV_IPC_WOF_CTRL                     =   2,
+    EV_IPC_WOF_VRT                      =   3,
+    EV_IPC_SET_PMCR                     =   4,
+    EV_PCB_SET_PMCR                     =   5,
+    EV_PSTATE_STOP                      =   6,
+    EV_SAFE_MODE                        =   7,
+    EV_PM_COMPLEX_SUSPEND               =   8,
+    EV_OCC_FAULT                        =   9,
+    EV_QME_FAULT                        =   10,
+    EV_XGPE_FAULT                       =   11,
+    EV_PVREF_FAULT                      =   12,
+    MAX_EVENT_TABLE_ENTRIES             =   13
+};
 
-//405 IPCs
-void pgpe_irq_ipc_405_start_stop(ipc_msg_t* cmd, void* arg);
-void pgpe_irq_ipc_405_clips(ipc_msg_t* cmd, void* arg);
-void pgpe_irq_ipc_405_set_pmcr(ipc_msg_t* cmd, void* arg);
-void pgpe_irq_ipc_405_wof_control(ipc_msg_t* cmd, void* arg);
-void pgpe_irq_ipc_405_wof_vfrt(ipc_msg_t* cmd, void* arg);
+enum EVENT_STATE
+{
+    EVENT_INACTIVE              = 0x00000000,
+    EVENT_PENDING               = 0x00000001,
+    EVENT_PENDING_ACTUATION     = 0x00000002
+};
+
+typedef void (*fn_event_handler)(void* args);
+
+//
+typedef struct event
+{
+    uint32_t status;
+    void* args;
+} event_t;
 
 
-void pgpe_irq_pcb1_handler();
-void pgpe_irq_fault_handler();
-void pgpe_irq_pbax_handler();
+//
+void pgpe_event_tbl_init();
+event_t* pgpe_event_tbl_get(uint32_t event_idx);
+uint32_t  pgpe_event_tbl_get_status(uint32_t event_idx);
+void* pgpe_event_tbl_get_args(uint32_t event_idx);
+void pgpe_event_tbl_set(uint32_t event_idx, uint32_t status, void* args);
+void pgpe_event_tbl_set_status(uint32_t event_idx, uint32_t status);
 
 #endif
