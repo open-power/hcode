0475fbf9f8d53ac4d483283d00f56925b494930b
Rahul Batra <rbatra@us.ibm.com>
2019-04-09 13:42:53 -0400

    PGPE: Basic Pstates for P10
    Change-Id: I5fd9fa51c3bef824540da529b1efbde53be7be2d
    Original-Change-Id: Id5a78ddd6581a0cc5372272ff9aa8dff09826a30
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75775
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_dpll.c
@@ -0,0 +1,71 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/ppe/pgpe/pgpe_dpll.c $            */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2019,2020                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+#include "pgpe_dpll.h"
+#include "pgpe_gppb.h"
+#include "pgpe_temp_constants.h"
+
+void pgpe_dpll_init()
+{
+}
+
+uint64_t pgpe_dpll_get_dpll()
+{
+    uint64_t dpll_freq;
+
+    GPE_GETSCOM(TPC_DPLL_FREQ_CTRL_REG, dpll_freq);
+    PK_TRACE("dpll=0x%08x%08x", dpll_freq >> 32, dpll_freq);
+
+    return dpll_freq;
+}
+
+void pgpe_dpll_write_dpll(uint64_t value)
+{
+    GPE_PUTSCOM(TPC_DPLL_FREQ_CTRL_REG, value);
+}
+
+uint32_t pgpe_dpll_get_pstate()
+{
+    dpll_freq_t dpll_freq;
+
+    GPE_GETSCOM(TPC_DPLL_FREQ_CTRL_REG, dpll_freq.value);
+
+    uint32_t pstate = pgpe_gppb_get(dpll_pstate0_value) - dpll_freq.fields.fmax;
+
+    return pstate;
+}
+
+void pgpe_dpll_write(uint32_t pstate)
+{
+    dpll_freq_t dpll_freq;
+    dpll_freq.value = 0;
+    dpll_freq.fields.fmax  = pgpe_gppb_get(dpll_pstate0_value) - pstate;
+    dpll_freq.fields.fmult = dpll_freq.fields.fmax;
+    dpll_freq.fields.fmin  = dpll_freq.fields.fmax;
+
+
+    PK_TRACE("dpll=0x%08x%08x", dpll_freq.value >> 32, dpll_freq.value);
+    GPE_PUTSCOM(TPC_DPLL_FREQ_CTRL_REG, dpll_freq.value);
+}
