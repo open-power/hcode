364d3159d240adecca15185234b476cfc891b23c
Rahul Batra <rbatra@us.ibm.com>
2019-04-09 13:42:53 -0400

    PGPE: Basic Pstates for P10
    Change-Id: I68a1277a78456cce0c2f6625e7c6cd0151364ee5
    Original-Change-Id: Id5a78ddd6581a0cc5372272ff9aa8dff09826a30
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/75775
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_event_process.c
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_event_process.c
@@ -25,69 +25,339 @@
 #include "pgpe_event_process.h"
 #include "pgpe_event_table.h"
 #include "pgpe_occ.h"
+#include "pgpe_pstate.h"
+#include "pgpe_header.h"
+#include "pgpe_dpll.h"
+#include "pgpe_gppb.h"
+#include "pgpe_avsbus_driver.h"
 
 //Local Functions
 void pgpe_process_pstate_start();
 void pgpe_process_pstate_stop();
-void pgpe_process_change_pmcr_owner();
+void pgpe_process_set_pmcr_owner();
 void pgpe_process_wof_enable();
 void pgpe_process_wof_disable();
 
 
-void pgpe_process_pstate_start_stop(void* args)
+void pgpe_process_pstate_start_stop(void* eargs)
 {
-    pgpe_occ_send_ipc_ack(EV_IPC_PSTATE_START_STOP, PGPE_RC_SUCCESS);
+    PK_TRACE("PEP: PS Stop");
+
+    ipc_msg_t* cmd = (ipc_msg_t*)eargs;
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_start_stop_t* args = (ipcmsg_start_stop_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
+
+    //Start
+    if (args->action == PGPE_ACTION_PSTATE_START)
+    {
+        //If NOT immediate mode, then process this IPC. Otherwise, we ACK back with success
+        if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+        {
+
+            //Check for PMCR onwer validity
+            if ((args->pmcr_owner == PMCR_OWNER_HOST) ||
+                (args->pmcr_owner == PMCR_OWNER_OCC)  ||
+                (args->pmcr_owner == PMCR_OWNER_CHAR))
+            {
+                //If pstate is NOT enabled, run pstate start protocol.
+                //Otherwise, only update owner
+                if(pgpe_pstate_is_pstate_enabled())
+                {
+                    pgpe_process_pstate_start();
+                }
+
+                pgpe_process_set_pmcr_owner(args->pmcr_owner);
+            }
+            else
+            {
+                args->msg_cb.rc = PGPE_RC_INVALID_PMCR_OWNER;
+            }
+        }
+        else
+        {
+            pgpe_pstate_set(pstate_status, PSTATE_STATUS_ENABLED);
+        }
+
+    }
+    //Stop
+    else
+    {
+        //If NOT immediate mode, then process this IPC. Otherwise, we ACK back with success
+        if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+        {
+            //Only run Pstate Stop protocol if Pstate is enabled.
+            //Otherwise, just ACK back
+            if(pgpe_pstate_is_pstate_enabled())
+            {
+                pgpe_process_pstate_stop();
+            }
+        }
+        else
+        {
+            pgpe_pstate_set(pstate_status, PSTATE_STATUS_DISABLED);
+        }
+    }
+
+
+    pgpe_occ_send_ipc_ack_cmd(cmd);
     pgpe_event_tbl_set_status(EV_IPC_PSTATE_START_STOP, EVENT_INACTIVE);
 }
 
 void pgpe_process_pstate_start()
 {
+    PK_TRACE("PEP: PS Start");
+    //Read DPLL frequency
+    uint32_t sync_pstate;
+    uint32_t voltage, move_frequency, vcs_before_vdd = 0;
+    dpll_stat_t dpll;
+    GPE_GETSCOM(TPC_DPLL_STAT_REG, dpll.value);
+
+    //2. Determine the highest pstate that matches with the read DPLL frequency
+    if (dpll.fields.freqout > pgpe_gppb_get(dpll_pstate0_value))
+    {
+        sync_pstate = 0;
+        move_frequency = -1;
+    }
+    else
+    {
+        sync_pstate = pgpe_gppb_get(dpll_pstate0_value) - dpll.fields.freqout;
+        move_frequency = 0;
+    }
+
+    PK_TRACE("PSS: DPLL=0x%x DPLL0=0x%x syncPS=0x%x", dpll.fields.freqout, pgpe_gppb_get(dpll_pstate0_value), sync_pstate);
+
+    //3. Clip the pstate
+    if (sync_pstate < pgpe_pstate_get(clip_min))
+    {
+        sync_pstate = pgpe_pstate_get(clip_min);
+        move_frequency = -1;
+    }
+
+    if (sync_pstate > pgpe_pstate_get(clip_max))
+    {
+        sync_pstate = pgpe_pstate_get(clip_max);
+        move_frequency = 1;
+    }
+
+    //Read the external VDD and VCS
+    pgpe_avsbus_voltage_read(pgpe_gppb_get(avs_bus_topology.vdd_avsbus_num),
+                             pgpe_gppb_get(avs_bus_topology.vdd_avsbus_rail),
+                             &voltage);
+    pgpe_pstate_set(vdd_curr_ext, voltage);
+    pgpe_avsbus_voltage_read(pgpe_gppb_get(avs_bus_topology.vcs_avsbus_num),
+                             pgpe_gppb_get(avs_bus_topology.vcs_avsbus_rail),
+                             &voltage);
+    pgpe_pstate_set(vcs_curr_ext, voltage);
+
+    //If frequency moving down, then adjust frequency
+    if (move_frequency < 0 )
+    {
+        pgpe_dpll_write(sync_pstate);
+    }
+
+    //Determine external VRM set points for sync pstate taking into account the
+    //system design parameters
+    pgpe_pstate_set(vdd_next, pgpe_pstate_intp_vdd_from_ps(sync_pstate, VPD_PT_SET_BIASED));
+    pgpe_pstate_set(vcs_next, pgpe_pstate_intp_vcs_from_ps(sync_pstate, VPD_PT_SET_BIASED ));
+    pgpe_pstate_set(vdd_next_uplift, pgpe_pstate_intp_vddup_from_ps(sync_pstate, VPD_PT_SET_BIASED));
+    pgpe_pstate_set(vcs_next_uplift, pgpe_pstate_intp_vcsup_from_ps(sync_pstate, VPD_PT_SET_BIASED));
+    pgpe_pstate_set(vdd_next_ext, pgpe_pstate_get(vdd_next) + pgpe_pstate_get(vdd_next_uplift));
+    pgpe_pstate_set(vcs_next_ext, pgpe_pstate_get(vcs_next) + pgpe_pstate_get(vcs_next_uplift));
+
+    //Perform voltage adjustment
+    //If new external VRM(VDD and VCS) set points different from present value, then
+    //move VDD and/or VCS
+    if ((pgpe_pstate_get(vdd_curr_ext) > pgpe_pstate_get(vdd_next_ext))
+        && (pgpe_pstate_get(vcs_curr_ext) > pgpe_pstate_get(vcs_next_ext)))
+    {
+        vcs_before_vdd = 0;
+    }
+    else if ((pgpe_pstate_get(vdd_curr_ext) < pgpe_pstate_get(vdd_next_ext))
+             && (pgpe_pstate_get(vcs_curr_ext) < pgpe_pstate_get(vcs_next_ext)))
+    {
+        vcs_before_vdd = 1;
+    }
+    else if ((pgpe_pstate_get(vdd_curr_ext) > pgpe_pstate_get(vdd_next_ext))
+             && (pgpe_pstate_get(vcs_curr_ext) < pgpe_pstate_get(vcs_next_ext)))
+    {
+        vcs_before_vdd = 1;
+    }
+    else if ((pgpe_pstate_get(vdd_curr_ext) < pgpe_pstate_get(vdd_next_ext))
+             && (pgpe_pstate_get(vcs_curr_ext) > pgpe_pstate_get(vcs_next_ext)))
+    {
+        vcs_before_vdd = 1;
+    }
+
+    if(vcs_before_vdd)
+    {
+        pgpe_avsbus_voltage_write(pgpe_gppb_get(avs_bus_topology.vcs_avsbus_num),
+                                  pgpe_gppb_get(avs_bus_topology.vcs_avsbus_rail),
+                                  pgpe_pstate_get(vcs_next_ext));
+        pgpe_avsbus_voltage_write(pgpe_gppb_get(avs_bus_topology.vdd_avsbus_num),
+                                  pgpe_gppb_get(avs_bus_topology.vdd_avsbus_rail),
+                                  pgpe_pstate_get(vdd_next_ext));
+    }
+    else
+    {
+        pgpe_avsbus_voltage_write(pgpe_gppb_get(avs_bus_topology.vdd_avsbus_num),
+                                  pgpe_gppb_get(avs_bus_topology.vdd_avsbus_rail),
+                                  pgpe_pstate_get(vdd_next_ext));
+        pgpe_avsbus_voltage_write(pgpe_gppb_get(avs_bus_topology.vcs_avsbus_num),
+                                  pgpe_gppb_get(avs_bus_topology.vcs_avsbus_rail),
+                                  pgpe_pstate_get(vcs_next_ext));
+    }
+
+    //If frequency moving up, then adjust frequency
+    if (move_frequency > 0 )
+    {
+        pgpe_dpll_write(sync_pstate);
+    }
+
+    //Enable the resonant clocks
+    //Setup DDS delay values
+    //Enable the DDS across all good cores
+    //Change pstate status to START
+    pgpe_pstate_set(pstate_status, PSTATE_STATUS_ENABLED);
 }
 
 void pgpe_process_pstate_stop()
 {
+    PK_TRACE("PEP: PS Stop");
+    //Disable resonant clocks
+    //DDS left untouched
+    //Disable WOF
+    //Disable WOV(undervolting/overvolting)
+    //Change pstate status to STOP
+    pgpe_pstate_set(pstate_status, PSTATE_STATUS_DISABLED);
 }
 
-void pgpe_process_change_pmcr_owner(void* args)
+void pgpe_process_set_pmcr_owner(PMCR_OWNER owner)
 {
+
+    PK_TRACE("PEP: PS Owner");
+    //Set the PMCR owner
+    pgpe_pstate_set(pmcr_owner, owner);
+
+    //\todo
+    //If PMCR owner CHAR or HOST
+    //Check that PMCR HW assist is enabled
+    //if assist not enabled,
+    //PGPE checks all the QME Flag[PMCR Ready] bits
+    //If check fails, critical error log, and give fail rc back to OCC
 }
 
-void pgpe_process_clip_update(void* args)
+void pgpe_process_clip_update(void* eargs)
 {
-    pgpe_occ_send_ipc_ack(EV_IPC_CLIP_UPDT, PGPE_RC_SUCCESS);
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_clip_update_t* args = (ipcmsg_clip_update_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
+
+    PK_TRACE("PEP: Clip Updt");
+
+    if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+    {
+        pgpe_pstate_set(clip_min, args->ps_val_clip_min);
+        pgpe_pstate_set(clip_max, args->ps_val_clip_max);
+
+        pgpe_pstate_compute();
+        pgpe_pstate_apply_clips();
+    }
+    else
+    {
+        pgpe_occ_send_ipc_ack_cmd((ipc_msg_t*)eargs);
+        pgpe_event_tbl_set_status(EV_IPC_CLIP_UPDT, EVENT_INACTIVE);
+    }
+}
+
+void pgpe_process_clip_update_w_ack(void* eargs)
+{
+    ipc_msg_t* cmd = (ipc_msg_t*)eargs;
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_clip_update_t* args = (ipcmsg_clip_update_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
+
+    PK_TRACE("PEP: Clip Updt w/ack");
+
+    if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+    {
+        pgpe_pstate_set(clip_min, args->ps_val_clip_min);
+        pgpe_pstate_set(clip_max, args->ps_val_clip_max);
+
+        pgpe_pstate_compute();
+        pgpe_pstate_apply_clips();
+    }
+
+    pgpe_occ_send_ipc_ack_cmd(cmd);
     pgpe_event_tbl_set_status(EV_IPC_CLIP_UPDT, EVENT_INACTIVE);
 }
 
-void pgpe_process_clip_update_w_ack(void* args)
+void pgpe_process_pmcr_request(void* eargs)
 {
-    pgpe_occ_send_ipc_ack(EV_IPC_CLIP_UPDT, PGPE_RC_SUCCESS);
-    pgpe_event_tbl_set_status(EV_IPC_CLIP_UPDT, EVENT_INACTIVE);
-}
+    PK_TRACE("PEP: PS PMCR");
+    ipc_msg_t* cmd = (ipc_msg_t*)eargs;
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_set_pmcr_t* args = (ipcmsg_set_pmcr_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
 
-void pgpe_process_pmcr_request(void* args)
-{
-    pgpe_occ_send_ipc_ack(EV_IPC_SET_PMCR, PGPE_RC_SUCCESS);
+    uint32_t i = 0;
+
+    if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+    {
+        for (i = 0; i < MAX_CORES; i++)
+        {
+            pgpe_pstate_set_ps_request(i, args->pmcr);
+        }
+
+        pgpe_pstate_compute();
+        pgpe_pstate_apply_clips();
+    }
+
+    pgpe_occ_send_ipc_ack_cmd(cmd);
     pgpe_event_tbl_set_status(EV_IPC_SET_PMCR, EVENT_INACTIVE);
 }
 
-void pgpe_process_wof_ctrl(void* args)
+void pgpe_process_wof_ctrl(void* eargs)
 {
-    pgpe_occ_send_ipc_ack(EV_IPC_WOF_CTRL, PGPE_RC_SUCCESS);
-    pgpe_event_tbl_set_status(EV_IPC_WOF_CTRL, EVENT_INACTIVE);
+    PK_TRACE("PEP: WOF CTRL");
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_wof_control_t* args = (ipcmsg_wof_control_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
+
+    if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+    {
+    }
+    else
+    {
+        pgpe_occ_send_ipc_ack_cmd((ipc_msg_t*)eargs);
+        pgpe_event_tbl_set_status(EV_IPC_WOF_CTRL, EVENT_INACTIVE);
+    }
 }
 
-void pgpe_process_wof_enable(void* args)
+void pgpe_process_wof_enable()
 {
 }
 
-void pgpe_process_wof_disable(void* args)
+void pgpe_process_wof_disable()
 {
 }
 
-void pgpe_process_wof_vrt(void* args)
+void pgpe_process_wof_vrt(void* eargs)
 {
-    pgpe_occ_send_ipc_ack(EV_IPC_WOF_VRT, PGPE_RC_SUCCESS);
-    pgpe_event_tbl_set_status(EV_IPC_WOF_VRT, EVENT_INACTIVE);
+    PK_TRACE("PEP: WOF VRT");
+    ipc_async_cmd_t* async_cmd = (ipc_async_cmd_t*)eargs;
+    ipcmsg_wof_vrt_t* args = (ipcmsg_wof_vrt_t*)async_cmd->cmd_data;
+    args->msg_cb.rc = PGPE_RC_SUCCESS; //Assume IPC will process ok. Any error case set other RCs
+
+    if((pgpe_header_get(g_pgpe_flags) & PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE) == 0)
+    {
+    }
+    else
+    {
+        pgpe_occ_send_ipc_ack_cmd((ipc_msg_t*)eargs);
+        pgpe_event_tbl_set_status(EV_IPC_WOF_VRT, EVENT_INACTIVE);
+    }
+
 }
 
 void pgpe_process_safe_mode(void* args)
