11cbe9c90e16c870b30f15c5daf11d6bfa437cf0
Ben Gass <bgass@us.ibm.com>
2019-04-18 15:00:47 -0400

    Various scom header file updates
    -Add chip, pau, and mi registers
    
    -Break each target header file into 16 sub files
    and evenly/deterministically distribute
    definitions between then.  Procedure owners
    can only include the sub files necessary to
    help with compile time.  Or they can continue
    to call the top level target header file which
    includes all the sub files.
    
    -Rename p10_st_errors.xml to p10_scomt_errors.xml
    
    -Fix briefs in example files
    
    -Disable mk files for examples to save build
    time
    
    Change-Id: I8b85fd5ddc9fc680a0d3830a2d7e3f45fdd8d0aa
    Original-Change-Id: I263214dc4036cdb61433ab3b7cf77eb8a864083d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76167
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Jenny Huynh <jhuynh@us.ibm.com>
    Reviewed-by: Nicholas Landi <nlandi@ibm.com>
    Reviewed-by: Jennifer A. Stofer <stofer@us.ibm.com>

---
+++ import/chips/p10/common/include/rmoldregs.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p10/common/include/rmoldregs.pl $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2019,2020
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+@dlst = ( "iohs", "mc", "mcc", "mi", "omi", "omic", "pau", "pec", "perv", "phb", "proc", "c", "eq" );
+
+foreach $dr (@dlst)
+{
+    $newest = `ls -ltr  $dr | tail -1`;
+    if ( $newest =~ /\s+(\d+):(\d+)\s+/ )
+    {
+        $ntime = "$1:$2";
+        @listdir = split( /\n/, `ls -ltr $dr` );
+        foreach $tf (@listdir)
+        {
+            if ( $tf !~ /$ntime/ && $tf =~ /(\S+).H/ )
+            {
+                system("git rm $dr/$1.H");
+                system("rm $dr/$1.H");
+            }
+        }
+    }
+}
