a3340bf1e6ecb61e56b2179d23dc17b24e626fc6
Joe McGill <jmcgill@us.ibm.com>
2019-04-28 15:46:50 -0400

    clock checking and scan/scom address xlate updates
    track pervasive scan region and SCOM ring hookups based on drop R VHDL
    and current Clock Voltage spec XLS
    
    match scan region updates
      FSI     added fsi0inv (region #5)
      TP      added dpllpau (region #8), dpllnest (region #9)
      N0      shifted pe1 to own region (region #4), added iopsi (region #6)
      N1      remove region #1 (mcd -> region #2, pe0 -> region #4)
      PCI     add pcs0..3 (region #2-#5), psm0..3 (region #6-#9)
      MC      remove region #2 (dl01 -> region #3, dl23 -> region #4,
              ioo0 -> region #5, ioo1 -> region #6)
      EQ      added clkadj (region #10)
      AXON    remove odl (region #3)
    
    match scom ring updates
      NMMU    extend match to second SCOM satellite (#1) on ring #3
      PEC,PHB correct n0/n1 instance numbering
              cover additional sat IDs (#4-#6) on PCI chiplet ring #2
      PAUC    rename from PPE
              remove non-IO PPE entries, P10 target will only cover IO PPEs
              add PTL, IO PPE matching rules (direct and per-bus indirect)
    	  support match for PAU chiplet pervasive resources
      PAU     update ring matching rules (#2,#3 for first pau unit in chiplet,
              #4,#5 for second)
      IOHS    limit ring match for direct AXON chiplet access to ring #4 (pdl)
              update matching rules for indirect PAU chiplet access via IOPPE,
              remove rules matching direct PAU chiplet access
      EQ      fix matching rules for EQ,C target split of ring space
              on PSCOM endpoint #1
      MC,MI   adjust ring ID (mc_0 shifted from #2 to #3)
              change satellite ID matches to cover MCBIST and PBI
      MCC     adjust ring ID (mc_0 shifted from #2 to #3), cover
              upstream and downstream TL layer registers as well
              as MCBIST/PBI indirect access to the channel facilities
      OMIC    add matches for IO PPE indirect per-group regs,
              MC direct access through omi rings (#5-#6) for shared DL regs
      OMI     add matches for IO PPE indirect per-lane regs,
              MC direct access through omi rings (#5-#6) for sub-channel
              specific DL regs (separate range for non PM and PM specific regs)
    
    update HWP metadata comments for scan xlate source files
    
    fix unitialized variables in p10_scom_xlate impacting clock domain
    analysis for chip target scoped input
    
    update unit test to cover matching changes
    
    Change-Id: I7010708c55c7514b0b60f93518ab31c4beec8103
    Original-Change-Id: I769a4d76266ffcf94d803887e73c6acdf57e5107
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76629
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>
    Reviewed-by: Joseph J. McGill <jmcgill@us.ibm.com>

--- import/chips/p10/common/scominfo/p10_scaninfo.C
+++ import/chips/p10/common/scominfo/p10_scaninfo.C
@@ -26,10 +26,9 @@
 /// @file p10_scaninfo.C
 /// @brief P10 scan address control functions for platform
 ///
-/// HWP Owner: jmcgill@us.ibm.com
-/// HWP Team: NEST
-/// HWP Level: 1
-/// HWP Consumed by: FSP/HB
+/// *HWP HW Maintainer: Joe McGill <jmcgill@us.ibm.com>
+/// *HWP FW Maintainer:
+/// *HWP Consumed by: Cronus, HWSV, HB
 ///
 
 #ifndef P10_SCANINFO_C
