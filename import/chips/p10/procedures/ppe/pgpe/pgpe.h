438fb56d9b50db4eb017a0f19a793706c208c1b3
Rahul Batra <rbatra@us.ibm.com>
2019-04-30 17:57:56 -0400

    PGPE10: Basic Pstates Set 2
    Change-Id: I9695aca5af0d7eb665ca24f76ac2c1ff2d736007
    Original-Change-Id: Ife4b34b9c5ee8362d1f269be7fd43b11833f6f31
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/76774
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Thi N. Tran <thi@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe.h
+++ import/chips/p10/procedures/ppe/pgpe/pgpe.h
@@ -26,17 +26,46 @@
 #ifndef __PGPE_H__
 #define __PGPE_H__
 
-#include "iota.h"
-#include "iota_trace.h"
-#include "ipc_api.h"
-#include "ipc_async_cmd.h"
-#include "pstate_pgpe_occ_api.h"
-#include "p10_pm_hcd_flags.h"
-#include "pgpe_temp_constants.h"
-#include "pstates_pgpe.H"
-#include "pstates_common.H"
-#include "gpehw_common.h"
-#include "ppe42_scom.h"
+#ifndef __PARSER_TOOL__
+
+    #include "iota.h"
+    #include "iota_trace.h"
+    #include "ipc_api.h"
+    #include "ipc_async_cmd.h"
+    #include "pstate_pgpe_occ_api.h"
+    #include "p10_pm_hcd_flags.h"
+    #include "pgpe_temp_constants.h"
+    #include "pstates_pgpe.H"
+    #include "pstates_common.H"
+    #include "gpehw_common.h"
+    #include "ppe42_scom.h"
+
+#endif
+
+#define CORE_MASK(core) \
+    (0x80000000 >> core)
+
+
+#define MAX_DATA_STRUCT 16
+
+enum  DATA_STRUCT_TABLE_IDX
+{
+    DATA_EVENT_MANAGER      = 0,
+    DATA_EVENT_TABLE        = 1,
+    DATA_PSTATE             = 2
+};
+
+typedef struct data_struct_entry
+{
+    uint32_t address;
+    uint32_t size;
+} data_struct_entry_t;
+
+typedef struct data_struct_table
+{
+    data_struct_entry_t entry[MAX_DATA_STRUCT];
+} data_struct_table_t;
+
 
 
 #endif //
