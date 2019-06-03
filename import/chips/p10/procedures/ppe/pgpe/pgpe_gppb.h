6d22f21d001a82a0636c5c533e83c02b62c1ba01
Rahul Batra <rbatra@us.ibm.com>
2019-06-03 18:15:11 -0400

    PGPE: DDS Calculation
    Change-Id: I6590cd748f3c8f3666c68025ed4f7b0bc33989ef
    Original-Change-Id: I8196e132b4c3cdfdbf142da7968565389238f38c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/78278
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_gppb.h
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_gppb.h
@@ -47,6 +47,9 @@ void pgpe_gppb_init();
 #define pgpe_gppb_get_ps_idc_slope(rail, pt_set, region) G_gppb->ps_dc_current_slopes[rail][pt_set][region]
 #define pgpe_gppb_get_iac_ps_slope(rail, pt_set, region) G_gppb->current_ps_slopes[rail][pt_set][region]
 #define pgpe_gppb_get_idc_ps_slope(rail, pt_set, region) G_gppb->current_ps_slopes[rail][pt_set][region]
+#define pgpe_gppb_get_dds_delay_ps_slope(pt_set, core, region) G_gppb->ps_dds_delay_slopes[pt_set][core][region]
+#define pgpe_gppb_get_dds_delay(core, idx) G_gppb->dds[core][idx].ddsc.fields.insrtn_dely
+#define pgpe_gppb_get_dds_cal_adj(core, idx) G_gppb->dds[core][idx].ddsc.fields.calb_adj
 #define pgpe_gppb_get_core_on_ratio_vdd() G_gppb->core_on_ratio_vdd
 #define pgpe_gppb_get_l3_on_ratio_vdd() G_gppb->l3_on_ratio_vdd
 #define pgpe_gppb_get_mma_on_ratio_vdd() G_gppb->mma_on_ratio_vdd
