cd0059e7c52bbde0536e485b0594620918f7809f
Yue Du <daviddu@us.ibm.com>
2019-06-19 13:19:51 -0500

    QME: Scominit support by QME Platform
    Change-Id: I76124170fa8ed8c424ed51001d6a2d9e1e30020c
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79208
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/qme/qme_files.mk
+++ import/chips/p10/procedures/ppe/qme/qme_files.mk
@@ -30,65 +30,69 @@ IMG-S-SOURCES   = cpmr_header.S
 
 QME-S-SOURCES   = qme_header.S
 
-QME-CPP-SOURCES = qme_main.C 
+QME-CPP-SOURCES = qme_main.C       \
+                  qme_stop_entry.C \
+                  qme_stop_exit.C 
          
-QME-C-SOURCES   = qme_addresses.c \
-                  qme_init.c \
-                  qme_irq_timer_events.c \
-                  qme_irq_doorbell_events.c
+QME-C-SOURCES   = qme_addresses.c           \
+                  qme_init.c                \
+                  qme_irq_timer_events.c    \
+                  qme_irq_doorbell_events.c \
+                  qme_irq_stop_events.c 
 
-STOP-C-SOURCES  = qme_irq_stop_events.c 
-
-STOP-CPP-SOURCES = qme_stop_entry.C \
-                   qme_stop_exit.C 
-                   
-CORECACHE-CPP-SOURCES = p10_hcd_corecache_clock_control.C \
-                        p10_hcd_corecache_power_control.C
-
-CACHE-CPP-SOURCES = p10_hcd_cache_poweron.C \
-                    p10_hcd_cache_reset.C \
-                    p10_hcd_cache_gptr_time_initf.C \
-                    p10_hcd_cache_repair_initf.C \
-                    p10_hcd_cache_arrayinit.C \
-                    p10_hcd_cache_initf.C \
-                    p10_hcd_cache_startclocks.C \
-                    p10_hcd_cache_scom_customize.C \
-                    p10_hcd_cache_poweroff.C \
-                    p10_hcd_cache_stopclocks.C \
-                    p10_hcd_l3_purge.C \
-                    p10_hcd_chtm_purge.C \
-                    p10_hcd_powerbus_purge.C 
-
-
-#TODO p10_hcd_cache_scominit.C
-                     
-CORE-CPP-SOURCES  = p10_hcd_core_poweron.C \
-                    p10_hcd_core_reset.C \
-                    p10_hcd_core_gptr_time_initf.C \
-                    p10_hcd_core_repair_initf.C \
-                    p10_hcd_core_arrayinit.C \
-                    p10_hcd_core_initf.C \
-                    p10_hcd_core_startclocks.C \
-                    p10_hcd_core_scom_customize.C \
-                    p10_hcd_core_poweroff.C \
-                    p10_hcd_core_stopclocks.C \
-                    p10_hcd_core_stopgrid.C \
-                    p10_hcd_core_startgrid.C \
-                    p10_hcd_core_vmin_disable.C \
-                    p10_hcd_core_vmin_enable.C \
-                    p10_hcd_core_shadows_disable.C \
-                    p10_hcd_core_shadows_enable.C \
-                    p10_hcd_l2_purge.C \
-                    p10_hcd_ncu_purge.C \
-                    p10_hcd_l2_tlbie_quiesce.C 
+SUBMODULE-CPP-SOURCES = p10_hcd_corecache_clock_control.C \
+                        p10_hcd_corecache_power_control.C \
+                        p10_fbc_utils.C 
+#                        p10_perv_sbe_cmn.C
 
 SCAN-CPP-SOURCES  = p10_qme_putringutils.C \
                     p10_qme_ring_traverse.C
-#TODO p10_hcd_core_scominit.C
 
-IMG_OBJECTS    = $(IMG-C-SOURCES:.c=.o) $(IMG-S-SOURCES:.S=.o)
-QME_OBJECTS    = $(QME-CPP-SOURCES:.C=.o) $(QME-C-SOURCES:.c=.o) $(QME-S-SOURCES:.S=.o)
-STOP_OBJECTS   = $(STOP-CPP-SOURCES:.C=.o) $(STOP-C-SOURCES:.c=.o)
-HWP_OBJECTS    = $(CORECACHE-CPP-SOURCES:.C=.o) $(CACHE-CPP-SOURCES:.C=.o) $(CORE-CPP-SOURCES:.C=.o) $(SCAN-CPP-SOURCES:.C=.o)
+CACHE-CPP-SOURCES = p10_hcd_l3_purge.C              \
+                    p10_hcd_chtm_purge.C            \
+                    p10_hcd_powerbus_purge.C        \
+                    p10_hcd_cache_stopclocks.C      \
+                    p10_hcd_cache_poweroff.C        \
+                    p10_hcd_cache_poweron.C         \
+                    p10_hcd_cache_reset.C           \
+                    p10_hcd_cache_gptr_time_initf.C \
+                    p10_hcd_cache_repair_initf.C    \
+                    p10_hcd_cache_arrayinit.C       \
+                    p10_hcd_cache_initf.C           \
+                    p10_hcd_cache_startclocks.C     \
+                    p10_hcd_cache_scominit.C        \
+                    p10_hcd_cache_scom_customize.C 
+                    
+CORE-CPP-SOURCES  = p10_hcd_l2_purge.C             \
+                    p10_hcd_l2_tlbie_quiesce.C     \
+                    p10_hcd_ncu_purge.C            \
+                    p10_hcd_core_shadows_disable.C \
+                    p10_hcd_core_stopclocks.C      \
+                    p10_hcd_core_stopgrid.C        \
+                    p10_hcd_core_vmin_enable.C     \
+                    p10_hcd_core_poweroff.C        \
+                    p10_hcd_core_poweron.C         \
+                    p10_hcd_core_reset.C           \
+                    p10_hcd_core_gptr_time_initf.C \
+                    p10_hcd_core_repair_initf.C    \
+                    p10_hcd_core_arrayinit.C       \
+                    p10_hcd_core_initf.C           \
+                    p10_hcd_core_vmin_disable.C    \
+                    p10_hcd_core_startgrid.C       \
+                    p10_hcd_core_startclocks.C     \
+                    p10_hcd_core_shadows_enable.C  \
+                    p10_hcd_core_scominit.C        \
+                    p10_hcd_core_scom_customize.C  
+
+
+QME_OBJECTS    = $(QME-CPP-SOURCES:.C=.o) \
+                 $(QME-C-SOURCES:.c=.o)   \
+                 $(QME-S-SOURCES:.S=.o)   \
+                 $(IMG-S-SOURCES:.S=.o) 
+
+HWP_OBJECTS    = $(SUBMODULE-CPP-SOURCES:.C=.o) \
+                 $(SCAN-CPP-SOURCES:.C=.o)      \
+                 $(CACHE-CPP-SOURCES:.C=.o)     \
+                 $(CORE-CPP-SOURCES:.C=.o)      
 
 endif
