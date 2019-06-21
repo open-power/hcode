e695f15db0f3afc5eb00ce9bc2fdecb17c97e1bb
Prasad Bg Ranganath <prasadbgr@in.ibm.com>
2019-06-21 08:08:59 -0500

    XGPE HCODE initial update
    Change-Id: Id3a608c4de4e32f461b5bdfb7dfb0af3b70bc7e8
    Original-Change-Id: Ib6806308ebda4ff150ee6b5bbba093b54b76552f
    RTC:200916
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/79325
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Gregory S. Still <stillgs@us.ibm.com>
    Reviewed-by: RAHUL BATRA <rbatra@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/xgpe/xgpe_files.mk
@@ -0,0 +1,33 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p10/procedures/ppe/xgpe/xgpe_files.mk $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2019,2021
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+ifdef IMAGE
+XGPE-C-SOURCES = xgpe_main.c \
+				xgpe_irq_priority_table.c \
+				xgpe_uih.c \
+				xgpe_irq_handlers.c \
+				xgpe_irq_handlers_fit.c \
+
+XGPE_OBJECTS = $(XGPE-C-SOURCES:.c=.o) 
+endif
