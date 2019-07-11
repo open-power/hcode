1e0aa92d75751dbca98ffcc64ff1e6ee213e5268
Rahul Batra <rbatra@us.ibm.com>
2019-07-11 14:58:09 -0400

    PGPE: Resonant Clock
    Change-Id: I57a620d1b060116d912101b793687cf7946b5302
    Original-Change-Id: Ia66c7203c24d60bb2c36f3602188ff8295307a19
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/80307
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_temp_structs.h
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_temp_structs.h
@@ -165,4 +165,209 @@ typedef union pmsr
     } fields;
 } pmsr_t;
 
+
+typedef union rcimr
+{
+    uint64_t value;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint64_t rp0        : 8;
+        uint64_t reserved0  : 3;
+        uint64_t rx0        : 5;
+        uint64_t rp1        : 8;
+        uint64_t reserved1  : 3;
+        uint64_t rx1        : 5;
+        uint64_t rp2        : 8;
+        uint64_t reserved2  : 3;
+        uint64_t rx2        : 5;
+        uint64_t rp3        : 8;
+        uint64_t reserved3  : 3;
+        uint64_t rx3        : 5;
+#else
+        uint64_t rx3        : 5;
+        uint64_t reserved3  : 3;
+        uint64_t rp3        : 8;
+        uint64_t rx2        : 5;
+        uint64_t reserved2  : 3;
+        uint64_t rp2        : 8;
+        uint64_t rx1        : 5;
+        uint64_t reserved1  : 3;
+        uint64_t rp1        : 8;
+        uint64_t rx0        : 5;
+        uint64_t reserved0  : 3;
+        uint64_t rp0        : 8;
+#endif
+    } fields;
+} rcimr_t;
+
+typedef union rctar
+{
+    uint64_t value;
+    uint8_t resclk_entry[8];
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+
+} rctar_t;
+
+typedef union rcptr
+{
+    uint64_t value;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint64_t target_pstate      : 8;
+        uint64_t pstate_ack_pending : 1;
+        uint64_t reserved           : 55;
+#else
+        uint64_t reserved           : 55;
+        uint64_t pstate_ack_pending : 1;
+        uint64_t target_pstate      : 8;
+#endif
+    } fields;
+} rcptr_t;
+
+typedef union rcmr
+{
+    uint64_t value;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint64_t step_enable            : 1;
+        uint64_t auto_ack_enable        : 1;
+        uint64_t auto_target_disable    : 1;
+        uint64_t auto_done_disable      : 1;
+        uint64_t step_delay             : 4;
+        uint64_t reserved0              : 27;
+        uint64_t core0_control_idx      : 5;
+        uint64_t reserved1              : 3;
+        uint64_t core1_control_idx      : 5;
+        uint64_t reserved2              : 3;
+        uint64_t core2_control_idx      : 5;
+        uint64_t reserved3              : 3;
+        uint64_t core3_control_idx      : 5;
+#else
+        uint64_t core3_control_idx      : 5;
+        uint64_t reserved3              : 3;
+        uint64_t core2_control_idx      : 5;
+        uint64_t reserved2              : 3;
+        uint64_t core1_control_idx      : 5;
+        uint64_t reserved1              : 3;
+        uint64_t core0_control_idx      : 5;
+        uint64_t reserved0              : 27;
+        uint64_t step_delay             : 4;
+        uint64_t auto_done_disable      : 1;
+        uint64_t auto_target_disable    : 1;
+        uint64_t auto_ack_enable        : 1;
+        uint64_t step_enable            : 1;
+#endif
+    } fields;
+} rcmr_t;
+
+typedef union opitnqa
+{
+    uint64_t value;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint64_t reserved0          : 13;
+        uint64_t pcb_intr_payload   : 18;
+        uint64_t reserved1          : 32;
+#else
+        uint64_t reserved1          : 32;
+        uint64_t pcb_intr_payload   : 18;
+        uint64_t reserved0          : 13;
+#endif
+    } fields;
+} opitnqa_t;
+
+typedef union opitnpra
+{
+    uint64_t value;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint32_t high_order;
+        uint32_t low_order;
+#else
+        uint32_t low_order;
+        uint32_t high_order;
+#endif
+    } words;
+    struct
+    {
+#ifdef _BIG_ENDIAN
+        uint64_t pcb_intr_type_pending0 : 1;
+        uint64_t pcb_intr_type_pending1 : 1;
+        uint64_t pcb_intr_type_pending2 : 1;
+        uint64_t pcb_intr_type_pending3 : 1;
+        uint64_t pcb_intr_type_pending4 : 1;
+        uint64_t pcb_intr_type_pending5 : 1;
+        uint64_t pcb_intr_type_pending6 : 1;
+        uint64_t pcb_intr_type_pending7 : 1;
+        uint64_t reserved               : 56;
+#else
+        uint64_t reserved               : 56;
+        uint64_t pcb_intr_type_pending7 : 1;
+        uint64_t pcb_intr_type_pending6 : 1;
+        uint64_t pcb_intr_type_pending5 : 1;
+        uint64_t pcb_intr_type_pending4 : 1;
+        uint64_t pcb_intr_type_pending3 : 1;
+        uint64_t pcb_intr_type_pending2 : 1;
+        uint64_t pcb_intr_type_pending1 : 1;
+        uint64_t pcb_intr_type_pending0 : 1;
+#endif
+    } fields;
+} opitnpra_t;
+
+
 #endif
