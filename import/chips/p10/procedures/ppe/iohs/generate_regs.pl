cee27e15cbaed8deab1a044a3ecb6a9a9694b767
Joe McGill <jmcgill@us.ibm.com>
2019-08-12 10:12:38 -0400

    IOPPE image build, source import updates
    - extend d7nmphy_import script to generate p10_io_ppe_regs content
      needed by HWP code -- pulls in perl scripts from 79354
    
    - update ioo, nv makfiles to set DPK_STACK_CHECK=0
    
    - initialize __pk_time_queue outside of PK_TRACE_SUPPORT ifdef - 473956
    
    - makefile updates to permit clean scratch build in IOO repository - 501425
    
    - modify standard_irq_config.h to include kernel specific app config
      file, std_register_addresses.h to differentiate QME address map
      from SBE, IOPPE - 501424, 501545
    
    Change-Id: I420ab1520286dafabe8d8f7ea7c2dcb0b09c52a7
    Original-Change-Id: Ibb8a625f9274606ead5d01b7430841d0a197c346
    CQ:HW473956
    CQ:HW501424
    CQ:HW501425
    CQ:HW501706
    CQ:HW501545
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/82076
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Benjamin Gass <bgass@us.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Douglas R Gilbert <dgilbert@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/iohs/generate_regs.pl
@@ -0,0 +1,86 @@
+#!/usr/bin/perl
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p10/procedures/ppe/iohs/generate_regs.pl $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2019,2020
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+
+use warnings;
+use strict;
+
+my $numargs = @ARGV;
+
+if ( $numargs < 4 )
+{
+    print "Required args: <hfile> <cfile> <section> <ppe_file>\n";
+    exit(1);
+}
+
+my $hfile   = $ARGV[0];
+my $cfile   = $ARGV[1];
+my $section = $ARGV[2];
+my $ppefile = $ARGV[3];
+my $suffix  = $ARGV[4];
+if ( !defined($suffix) || ( $suffix eq "-" ) )
+{
+    $suffix = "";
+}
+
+open( HBUF, ">>${hfile}" ) || die "Could not write file: $hfile";
+open( CBUF, ">>${cfile}" ) || die "Could not write file: $cfile";
+
+print HBUF "\n";
+print HBUF "extern p10_io_ppe_cache ${section};\n";
+print HBUF "\n";
+
+print CBUF "\n";
+print CBUF "p10_io_ppe_cache ${section}(&p10_io_phy_ppe_scom_regs, ${section}_start);\n";
+print CBUF "\n";
+
+open( BUF, $ppefile ) || die "Could not open ppe file: $ppefile";
+
+my $register;
+my $addr;
+my $shift;
+my $mask;
+while ( my $line = <BUF> )
+{
+
+    if ( $line =~ /\#define\s+(\S+)_addr\s+(\S+)/ )
+    {
+        $register = $1;
+        $addr     = $2;
+    }
+    if ( $line =~ /\#define\s+(\S+)_shift\s+(\S+)/ )
+    {
+        $shift = $2;
+    }
+    if ( $line =~ /\#define\s+(\S+)_mask\s+(\S+)/ )
+    {
+        $mask = $2;
+        print HBUF "extern p10_io_ppe_sram_reg p10_io_ppe_${register}${suffix};\n";
+        print CBUF "p10_io_ppe_sram_reg p10_io_ppe_${register}${suffix}(&${section}, $addr, $mask, $shift);\n";
+    }
+}
+
+close(HBUF);
+close(CBUF);
