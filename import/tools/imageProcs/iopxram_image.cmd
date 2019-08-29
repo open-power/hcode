1de06a6731c988b2d1c4bed3447424cc6227af46
Thi Tran <thi@us.ibm.com>
2019-08-29 14:01:01 -0400

    HWP supports: Import and build IOP instruction code into p10.hw_image.bin
    Change-Id: If578d310be65aa359235e8bbb8592d08eeee4451
    Original-Change-Id: I22000231bd829ab1dea2adbcff93705f748ef570
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83100
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: Claus M Olsen <cmolsen@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Joseph J McGill <jmcgill@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

---
+++ import/tools/imageProcs/iopxram_image.cmd
@@ -0,0 +1,58 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* COPYRIGHT 2019                                                         */
+/* IBM_PROLOG_END_TAG                                                     */
+
+// Need to do this so that elf32-powerpc is not modified!
+#undef powerpc
+
+#define IOPXRAM_IMAGE_ORIGIN 0x80000000
+OUTPUT_FORMAT(elf32-powerpc);
+
+SECTIONS
+{
+    . = IOPXRAM_IMAGE_ORIGIN;
+    _iopxram_image_origin = . - 0;
+    ////////////////////////////////
+    // Header
+    ////////////////////////////////
+    . = ALIGN(1);
+    _header_origin = .;
+    _header_offset = . - _iopxram_image_origin;
+    .header . : { *(.header) }
+    _header_size = . - _header_origin;
+
+    ////////////////////////////////
+    // TOC
+    ////////////////////////////////
+    . = ALIGN(4);
+    _toc_origin = .;
+    _toc_offset = . - _iopxram_image_origin;
+    .toc . : { *(.toc) }
+    _toc_size = . - _toc_origin;
+
+    ////////////////////////////////
+    // STRINGS
+    ////////////////////////////////
+    . = ALIGN(1);
+    _strings_origin = .;
+    _strings_offset = . - _iopxram_image_origin;
+    .strings . : { *(.strings) }
+    _strings_size = . - _strings_origin;
+
+    ////////////////////////////////
+    // IOP_FW image
+    ////////////////////////////////
+    . = ALIGN(8);
+    _iop_fw_origin = .;
+    _iop_fw_offset = . - _iopxram_image_origin;
+    .iop_fw . : { *(.iop_fw) }
+    _iop_fw_size = . - _iop_fw_origin;
+
+    ////////////////////////////////
+    // end of the image
+    ////////////////////////////////
+    . = ALIGN(8);
+    _iopxram_image_size = . - _iopxram_image_origin;
+    _iopxram_image_end = . - 0;
+
+}
