20bfd4400669c695c283d48836d8f77e26c6d070
Prem Shanker Jha <premjha2@in.ibm.com>
2019-09-16 02:03:21 -0500

    XGPE Boot: Code infrastructure for XGPE boot.
    Commit accomplishes following :
        - porting of P9 XGPE bootcopier to P10
        - porting of P9 XGPE bootloader to P10
        - extracts XPMR Header from hardware image and places in HOMER
        - extracts XGPE BC and PGPE BL and places in HOMER
        - extracts XGPE hcode and places in HOMER
        - updates hcode image build wrapper for verification
    
    Change-Id: I00ff48ca03a77b7e0a23610e41da9f095aec2e1c
    Original-Change-Id: I619b0481ef65e4607d89d4cfe35f6aa9dc44c626
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/83961
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

---
+++ import/chips/p10/procedures/ppe/xgpe/boot/topfiles.mk
@@ -0,0 +1,39 @@
+# IBM_PROLOG_BEGIN_TAG
+# This is an automatically generated prolog.
+#
+# $Source: import/chips/p10/procedures/ppe/xgpe/boot/topfiles.mk $
+#
+# OpenPOWER EKB Project
+#
+# COPYRIGHT 2019,2021
+# [+] International Business Machines Corp.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+#
+# IBM_PROLOG_END_TAG
+TOP-C-SOURCES = 
+TOP-S-SOURCES =  xgpe_boot_loader.S xgpe_boot_copier.S
+
+TOP_OBJECTS = $(TOP-C-SOURCES:.c=.o) $(TOP-S-SOURCES:.S=.o)
+
+BOOT-LOADER-C-SOURCES = 
+BOOT-LOADER-S-SOURCES =  xgpe_boot_loader.S 
+
+BOOT_LOADER_OBJECTS = $(BOOT-LOADER-C-SOURCES:.c=.o) $(BOOT-LOADER-S-SOURCES:.S=.o)
+
+
+BOOT-COPIER-C-SOURCES = 
+BOOT-COPIER-S-SOURCES =  xgpe_boot_copier.S 
+
+BOOT_COPIER_OBJECTS = $(BOOT-COPIER-C-SOURCES:.c=.o) $(BOOT-COPIER-S-SOURCES:.S=.o)
