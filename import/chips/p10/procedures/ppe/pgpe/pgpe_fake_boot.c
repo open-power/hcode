697b934c123ae28a5769c190c6e21aca99f642eb
Rahul Batra <rbatra@us.ibm.com>
2019-10-09 23:21:36 -0400

    PM: Attributes/GPPB/PGPE Header updates
    Change-Id: Iace7c1a523c3e3443e57c85adf7f714c20329272
    Original-Change-Id: I5537ded79fe7ce24ad5c9366d423d4fa3b571b21
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/85103
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_fake_boot.c
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_fake_boot.c
@@ -146,12 +146,12 @@ void pgpe_fake_boot_pgpe_header()
     //pgpe_flags
     pgpe_header->g_pgpe_flags = 0;
 #if FAKE_BOOT_WOF_ENABLE == 1
-    pgpe_header->g_pgpe_flags |= PGPE_FLAG_WOF_ENABLE;
+    //pgpe_header->g_pgpe_flags |= PGPE_FLAG_WOF_ENABLE;
 #endif
 #if FAKE_BOOT_IMMEDIATE_MODE == 1
-    pgpe_header->g_pgpe_flags |= PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE;
+    //pgpe_header->g_pgpe_flags |= PGPE_FLAG_OCC_IPC_IMMEDIATE_MODE;
 #if FAKE_BOOT_WOF_ENABLE == 1
-    pgpe_header->g_pgpe_flags |= PGPE_FLAG_WOF_IPC_IMMEDIATE_MODE;
+    //pgpe_header->g_pgpe_flags |= PGPE_FLAG_WOF_IPC_IMMEDIATE_MODE;
 #endif
 #endif
     //timebase_hz
