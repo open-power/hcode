c4226f5b285fa06bb1138e9efdbd6d4c0ad20699
Yue Du <daviddu@us.ibm.com>
2019-11-21 11:40:34 -0600

    VBU: EQ_STOPCLOCKS
    Set #3 - Adds EQ clockstops selection to p10_stopclocks and its wrapper.
    Set #6 - Change target to 'eq' when calling p10_hcd_eq_stopclocks.C
             from wrapper and p10_stopclocks.C
    
    Change-Id: I7f422e29c710e818698d91f7708067432c4469c6
    Original-Change-Id: Iff2ced9df21603a23909e3fb7201bdd4e96065d9
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/87586
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Dev-Ready: Brent Wieman <bwieman@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Joachim Fenkes <fenkes@de.ibm.com>
    Reviewed-by: Thi N Tran <thi@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

---
+++ import/chips/p10/procedures/hwp/corecache/p10_hcd_eq_stopclocks.C
@@ -0,0 +1,80 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/hwp/corecache/p10_hcd_eq_stopclocks.C $ */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2018,2020                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+///
+/// @file  p10_hcd_eq_stopclocks.C
+/// @brief
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:Cronus
+// *HWP Level              : 2
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_eq_stopclocks.H"
+#include "p10_hcd_corecache_clock_control.H"
+#include "p10_hcd_common.H"
+
+#include "p10_scom_eq.H"
+using namespace scomt::eq;
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_eq_stopclocks
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_eq_stopclocks(
+    const fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target)
+{
+    fapi2::buffer<uint64_t> l_scomData = 0;
+
+    FAPI_INF(">>p10_hcd_eq_stopclocks");
+
+    FAPI_DBG("Disable ALL EQ Regional PSCOMs via CPLT_CTRL3[4-18]");
+    FAPI_TRY( HCD_PUTSCOM_Q( i_target, CPLT_CTRL3_WO_CLEAR, SCOM_LOAD32H( HCD_CLK_REGION_ALL ) ) );
+
+    FAPI_DBG("Enable ALL EQ Regional Fences via CPLT_CTRL1[4-18]");
+    FAPI_TRY( HCD_PUTSCOM_Q( i_target, CPLT_CTRL1_WO_OR, SCOM_LOAD32H( HCD_CLK_REGION_ALL ) ) );
+
+    FAPI_TRY( p10_hcd_corecache_clock_control( i_target, HCD_CLK_REGION_ALL, HCD_CLK_STOP ) );
+
+fapi_try_exit:
+
+    FAPI_INF("<<p10_hcd_eq_stopclocks");
+
+    return fapi2::current_err;
+}
