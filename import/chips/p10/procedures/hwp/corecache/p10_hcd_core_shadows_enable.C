2332316fc039b1ae55946475002fc245d9385e70
Greg Still <stillgs@us.ibm.com>
2020-01-09 22:45:55 -0500

    STOP: manage enablement timefac shadow based on TOD state
    - QME Hcode: add a QME Flag bit to reflect the TOD state (running/not)
    - QME Hcode: conditionally enable the timefac shadow hardware based on flag
    - p10_tod_init:  upon TOD setup completion, multicast this state to all QMEs
    - p10_pm_qme_init: reflect the TOD state to the QMEs to cover the possible case
      that the TOD gets disabled.
    - Use GETMMIO_Q macros for flag register
    
    Change-Id: I637f3c85a2fc2594eecd7621e164eeb1d7bc8c3d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/89550
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/hwp/corecache/p10_hcd_core_shadows_enable.C
+++ import/chips/p10/procedures/hwp/corecache/p10_hcd_core_shadows_enable.C
@@ -45,14 +45,22 @@
 
 #include "p10_hcd_core_shadows_enable.H"
 #include "p10_hcd_common.H"
+#include "p10_pm_hcd_flags.h"
 
 #ifdef __PPE_QME
     #include "p10_ppe_c.H"
+    #include "p10_ppe_eq.H"
     using namespace scomt::ppe_c;
+    using namespace scomt::ppe_eq;
+    #define QME_FLAGS_TOD_COMPLETE QME_FLAGS_TOD_SETUP_COMPLETE
 #else
     #include "p10_scom_c.H"
+    #include "p10_scom_eq.H"
     using namespace scomt::c;
+    using namespace scomt::eq;
+    #define QME_FLAGS_TOD_COMPLETE p10hcd::QME_FLAGS_TOD_SETUP_COMPLETE
 #endif
+
 
 //------------------------------------------------------------------------------
 // Constant Definitions
@@ -82,80 +90,97 @@ p10_hcd_core_shadows_enable(
 
     FAPI_INF(">>p10_hcd_core_shadows_enable");
 
-    FAPI_DBG("Assert CORE_INTR_SAMPLE/REFRESH_PMSR via PCR_SCSR[18/23]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_SCSR_WO_OR, MMIO_LOAD32H( BIT32(18) | BIT32(23) ) ) );
+    fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > l_eq_target =
+        i_target.getParent < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > ();
 
-    FAPI_DBG("Assert XFER_START via PCR_TFCSR[0]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_TFCSR_WO_OR, MMIO_1BIT(0) ) );
+    FAPI_TRY(HCD_GETMMIO_Q( l_eq_target, QME_FLAGS_RW, l_mmioData ) );
 
-    FAPI_DBG("Enable CORE_SHADOW via CUCR[0]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CUCR_WO_OR, MMIO_1BIT(0) ) );
-
-    FAPI_DBG("Wait for FTC/PP/DPT_SHADOW_STATE to be Idle via CUCR[33-35,40-41,45-46]");
-    l_timeout = HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS /
-                HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_HW_NS;
-
-    do
+    if ( l_mmioData & BIT32( QME_FLAGS_TOD_COMPLETE ) )
     {
-        FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(CPMS_CUCR), l_mmioData ) );
 
-        // use multicastAND to check 0
-        MMIO_GET32L(l_shadow_states);
+        FAPI_DBG("Assert CORE_INTR_SAMPLE/REFRESH_PMSR via PCR_SCSR[18/23]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_SCSR_WO_OR, MMIO_LOAD32H( BIT32(18) | BIT32(23) ) ) );
 
-        if( !( l_shadow_states & ( BITS64SH(33, 3) | BITS64SH(40, 2) | BITS64SH(45, 2) ) ) )
+        FAPI_DBG("Assert XFER_START via PCR_TFCSR[0]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_TFCSR_WO_OR, MMIO_1BIT(0) ) );
+
+        FAPI_DBG("Enable CORE_SHADOW via CUCR[0]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CUCR_WO_OR, MMIO_1BIT(0) ) );
+
+        FAPI_DBG("Wait for FTC/PP/DPT_SHADOW_STATE to be Idle via CUCR[33-35,40-41,45-46]");
+        l_timeout = HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS /
+                    HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_HW_NS;
+
+        do
         {
-            break;
+            FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(CPMS_CUCR), l_mmioData ) );
+
+            // use multicastAND to check 0
+            MMIO_GET32L(l_shadow_states);
+
+            if( !( l_shadow_states & ( BITS64SH(33, 3) | BITS64SH(40, 2) | BITS64SH(45, 2) ) ) )
+            {
+                break;
+            }
+
+            fapi2::delay(HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_HW_NS,
+                         HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_SIM_CYCLE);
         }
+        while( (--l_timeout) != 0 );
 
-        fapi2::delay(HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_HW_NS,
-                     HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_DELAY_SIM_CYCLE);
+        FAPI_ASSERT((l_timeout != 0),
+                    fapi2::SHADOW_ENA_CORE_SHADOW_STATE_TIMEOUT()
+                    .set_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS(HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS)
+                    .set_CPMS_CUCR(l_mmioData)
+                    .set_CORE_TARGET(i_target),
+                    "ERROR: Shadow Enable FTC/PP/DPT Shadow State Timeout");
+
+        FAPI_DBG("Wait on XFER_SENT_DONE via PCR_TFCSR[33]");
+        l_timeout = HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS /
+                    HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS;
+
+        do
+        {
+            FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR), l_mmioData ) );
+
+            // use multicastAND to check 1
+            if( MMIO_GET(MMIO_LOWBIT(33)) == 1 )
+            {
+                break;
+            }
+
+            fapi2::delay(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS,
+                         HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_SIM_CYCLE);
+        }
+        while( (--l_timeout) != 0 );
+
+        FAPI_ASSERT((l_timeout != 0),
+                    fapi2::SHADOW_ENA_XFER_SENT_DONE_TIMEOUT()
+                    .set_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS)
+                    .set_QME_TFCSR(l_mmioData)
+                    .set_CORE_TARGET(i_target),
+                    "ERROR: Shadow Enable Xfer Sent Done Timeout");
+
+#ifndef XFER_SENT_DONE_DISABLE
+
+        FAPI_DBG("Drop XFER_SENT_DONE via PCR_TFCSR[33]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR_WO_CLEAR), MMIO_1BIT( MMIO_LOWBIT(33) ) ) );
+
+#endif
+
+        FAPI_DBG("Drop CTFS_WKUP_ENABLE via PCR_SCSR[27]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_SCSR_WO_CLEAR, MMIO_1BIT(27) ) );
+
+        // P10 HW Spec requires 4 ppe cycle delay (to account for staging delay)
+        // between asserting CUCR[0] above and then asserting CUCR[1] below
+        // Extra poll loop above for XFER_SENT_DONE will be enough for 4 cycles
+        FAPI_DBG("Enable CORE_SAMPLE via CUCR[1]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CUCR_WO_OR, MMIO_1BIT(1) ) );
     }
-    while( (--l_timeout) != 0 );
-
-    FAPI_ASSERT((l_timeout != 0),
-                fapi2::SHADOW_ENA_CORE_SHADOW_STATE_TIMEOUT()
-                .set_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS(HCD_SHADOW_ENA_CORE_SHADOW_STATE_POLL_TIMEOUT_HW_NS)
-                .set_CPMS_CUCR(l_mmioData)
-                .set_CORE_TARGET(i_target),
-                "ERROR: Shadow Enable FTC/PP/DPT Shadow State Timeout");
-
-    FAPI_DBG("Wait on XFER_SENT_DONE via PCR_TFCSR[33]");
-    l_timeout = HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS /
-                HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS;
-
-    do
+    else
     {
-        FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR), l_mmioData ) );
-
-        // use multicastAND to check 1
-        if( MMIO_GET(MMIO_LOWBIT(33)) == 1 )
-        {
-            break;
-        }
-
-        fapi2::delay(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS,
-                     HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_SIM_CYCLE);
+        FAPI_INF("TOD not enabled.  Skipping enablement of TimeFac Shadow");
     }
-    while( (--l_timeout) != 0 );
-
-    FAPI_ASSERT((l_timeout != 0),
-                fapi2::SHADOW_ENA_XFER_SENT_DONE_TIMEOUT()
-                .set_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS)
-                .set_QME_TFCSR(l_mmioData)
-                .set_CORE_TARGET(i_target),
-                "ERROR: Shadow Enable Xfer Sent Done Timeout");
-
-    FAPI_DBG("Drop XFER_SENT_DONE via PCR_TFCSR[33]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR_WO_CLEAR), MMIO_1BIT( MMIO_LOWBIT(33) ) ) );
-
-    FAPI_DBG("Drop CTFS_WKUP_ENABLE via PCR_SCSR[27]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_SCSR_WO_CLEAR, MMIO_1BIT(27) ) );
-
-    // P10 HW Spec requires 4 ppe cycle delay (to account for staging delay)
-    // between asserting CUCR[0] above and then asserting CUCR[1] below
-    // Extra poll loop above for XFER_SENT_DONE will be enough for 4 cycles
-    FAPI_DBG("Enable CORE_SAMPLE via CUCR[1]");
-    FAPI_TRY( HCD_PUTMMIO_C( i_target, CPMS_CUCR_WO_OR, MMIO_1BIT(1) ) );
 
     //TODO Program calibrated DDS delay
 
