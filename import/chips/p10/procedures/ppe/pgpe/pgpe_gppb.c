630b87e082aec64f315627d6bae4d07c10d16874
Rahul Batra <rbatra@us.ibm.com>
2020-01-10 12:50:38 -0500

    PM: Pstate Dump Tool
    Change-Id: Idbd8b2887860b68be3c64f782321601b277a97d9
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/89515
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: Jennifer A Stofer <stofer@us.ibm.com>

--- import/chips/p10/procedures/ppe/pgpe/pgpe_gppb.c
+++ import/chips/p10/procedures/ppe/pgpe/pgpe_gppb.c
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER EKB Project                                                  */
 /*                                                                        */
-/* COPYRIGHT 2019                                                         */
+/* COPYRIGHT 2019,2020                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -26,11 +26,17 @@
 
 #include "pgpe_gppb.h"
 #include "pgpe_header.h"
+#include "pstates_table.H"
+#include "pgpe_pstate.h"
+#include "pstate_pgpe_occ_api.h"
 
-void pgpe_gppb_pstate_tbl();
+void pgpe_gppb_raw_pstate_tbl(PstateTable_t* tbl);
+void pgpe_gppb_biased_pstate_tbl(PstateTable_t* tbl);
 void pgpe_gppb_occ_tbl();
 
 GlobalPstateParmBlock_t* G_gppb;
+GeneratedPstateInfo_t* G_gpi;
+
 
 //
 //
@@ -42,23 +48,86 @@ void pgpe_gppb_init()
     void* gppb_sram_offset = (void*)G_pgpe_header_data->g_pgpe_gppb_sram_addr;//GPPB Sram Offset
     G_gppb = (GlobalPstateParmBlock_t*)gppb_sram_offset;
     PK_TRACE("GPPB: Init G_gppb=0x%x", (uint32_t)G_gppb);
+    PK_TRACE("GPPB: Init occ_cmpl_freq=0x%x", (uint32_t)G_gppb->occ_complex_frequency_mhz);
 
-    pgpe_gppb_pstate_tbl();
+#if GENERATE_HOMER_TABLES == 1
+    //Fill out GeneratedPstateInfo structure
+    PK_TRACE("GPPB: genPsTblMemOff=0x%08x", (uint32_t)G_pgpe_header_data->g_pgpe_genPsTableMemOffset);
+    G_gpi = (GeneratedPstateInfo_t*)G_pgpe_header_data->g_pgpe_genPsTableMemOffset;
+    G_gpi->magic = GEN_PSTATES_TBL_MAGIC;
+    G_gpi->marker_gppb_end = 0xdead0000;
+    G_gpi->globalppb = *G_gppb;
+    G_gpi->pstate0_frequency_khz = G_gppb->reference_frequency_khz;
+    G_gpi->highest_pstate = G_gppb->operating_points_set[VPD_PT_SET_BIASED_SYSP][POWERSAVE].pstate;
+    G_gpi->marker_raw_pstates_end = 0xdead0001;
+
+    //Clear the IPE bit because we are about to do 1B and 2B operations to main memory
+    uint32_t msr = mfmsr();
+    mtmsr(msr & ~MSR_IPE);
+
+    pgpe_gppb_raw_pstate_tbl((PstateTable_t*)(&G_gpi->raw_pstates));
+    pgpe_gppb_biased_pstate_tbl((PstateTable_t*)(&G_gpi->biased_pstates));
     pgpe_gppb_occ_tbl();
+
+    mtmsr(msr);//Restore MSR
+
+#endif
 }
 
 //
 //Generate Pstate Table
 //
-//\\TBD
-void pgpe_gppb_pstate_tbl()
+void pgpe_gppb_raw_pstate_tbl(PstateTable_t* tbl)
 {
+    int32_t p;
+    uint32_t freq_khz_offset = 0;
+
+    for (p = 0; p <= G_gppb->operating_points_set[VPD_PT_SET_RAW][POWERSAVE].pstate; p++)
+    {
+        tbl[p].pstate = p;
+        tbl[p].frequency_mhz = (G_gppb->reference_frequency_khz - freq_khz_offset) / 1000;
+        tbl[p].external_vdd_mv = pgpe_pstate_intp_vdd_from_ps(p, VPD_PT_SET_BIASED_SYSP);
+        tbl[p].effective_vdd_mv = pgpe_pstate_intp_vdd_from_ps(p, VPD_PT_SET_RAW);
+        freq_khz_offset += G_gppb->frequency_step_khz;
+        //\TODO Need Update spec as to what else needs to be in here
+    }
+
+    PK_TRACE("INIT: Generated Raw Pstate Tbl");
 }
 
+void pgpe_gppb_biased_pstate_tbl(PstateTable_t* tbl)
+{
+    int32_t p;
+    uint32_t freq_khz_offset = 0;
+
+    for (p = 0; p <= G_gppb->operating_points_set[VPD_PT_SET_BIASED_SYSP][POWERSAVE].pstate; p++)
+    {
+        tbl[p].pstate = p;
+        tbl[p].frequency_mhz = (G_gppb->reference_frequency_khz - freq_khz_offset) / 1000;
+        tbl[p].external_vdd_mv = pgpe_pstate_intp_vdd_from_ps(p, VPD_PT_SET_BIASED_SYSP);
+        tbl[p].effective_vdd_mv = pgpe_pstate_intp_vdd_from_ps(p, VPD_PT_SET_BIASED);
+        freq_khz_offset += G_gppb->frequency_step_khz;
+        //\TODO Need Update spec as to what else needs to be in here
+    }
+
+    PK_TRACE("INIT: Generated Biased Pstate Tbl");
+}
 //
 //Generate OCC Table
 //
-//\\TBD
 void pgpe_gppb_occ_tbl()
 {
+    int p;
+
+    OCCPstateTable_t* opst = (OCCPstateTable_t*)G_pgpe_header_data->g_pgpe_occ_pstables_sram_addr;
+    opst->entries = (G_pgpe_header_data->g_pgpe_occ_pstables_len) / sizeof(OCCPstateTable_entry_t);
+
+    for (p = 0; p < opst->entries; p++)
+    {
+        opst->table[p].pstate = G_gpi->biased_pstates[p].pstate;
+        opst->table[p].frequency_mhz = G_gpi->biased_pstates[p].frequency_mhz ;
+        //\TODO Need Update spec as to what else needs to be in here
+    }
+
+    PK_TRACE("INIT: Generated OCC Tbl");
 }
