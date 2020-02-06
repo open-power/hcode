81898ba44c8cde6d29de8f96a3adcbe0eb5897d4
Sumit Kumar <sumit_kumar@in.ibm.com>
2020-02-06 11:19:03 -0600

    Fix in ipl_image_tool to support OP-Build
    Change-Id: Ic13f732bc0088fafbd525008e6087165ac87f6ec
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/91250
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Reviewed-by: Chirag Sharma <chirshar@in.ibm.com>
    Reviewed-by: Claus M Olsen <cmolsen@us.ibm.com>
    Reviewed-by: Sumit Kumar <sumit_kumar@in.ibm.com>

--- import/tools/imageProcs/ipl_image_tool.mk
+++ import/tools/imageProcs/ipl_image_tool.mk
@@ -22,14 +22,31 @@
 # permissions and limitations under the License.
 #
 # IBM_PROLOG_END_TAG
+
+ifneq ($(OPENPOWER_BUILD),)
+EXE = ipl_image_tool
+OBJS += $(EXE).o
+$(EXE)_DEPLIBS+=p10_ipl_image
+$(EXE)_DEPLIBS+=common_ringId
+$(EXE)_DEPLIBS+=p10_tor
+$(EXE)_DEPLIBS+=p10_ringId
+$(EXE)_DEPLIBS+=p10_scan_compression
+$(EXE)_DEPLIBS+=p10_ddco
+$(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/chips/common/utils/imageProcs)
+$(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/chips/p10/utils/imageProcs)
+$(call ADD_EXE_INCDIR,$(EXE),$(ROOTPATH)/hwpf/fapi2/include)
+$(call BUILD_EXE)
+else
 WRAPPER = ipl_image_tool
+OBJS += $(WRAPPER).o
 $(WRAPPER)_DEPLIBS+=p10_ipl_image
 $(WRAPPER)_DEPLIBS+=common_ringId
 $(WRAPPER)_DEPLIBS+=p10_tor
 $(WRAPPER)_DEPLIBS+=p10_ringId
 $(WRAPPER)_DEPLIBS+=p10_scan_compression
 $(WRAPPER)_DEPLIBS+=p10_ddco
-$(call ADD_EXE_INCDIR,$(WRAPPER),$(ROOTPATH)/chips/common/utils/imageProcs)
-$(call ADD_EXE_INCDIR,$(WRAPPER),$(ROOTPATH)/chips/p10/utils/imageProcs)
-$(call ADD_EXE_INCDIR,$(WRAPPER),$(ROOTPATH)/hwpf/fapi2/include)
+$(call ADD_WRAPPER_INCDIR,$(WRAPPER),$(ROOTPATH)/chips/common/utils/imageProcs)
+$(call ADD_WRAPPER_INCDIR,$(WRAPPER),$(ROOTPATH)/chips/p10/utils/imageProcs)
+$(call ADD_WRAPPER_INCDIR,$(WRAPPER),$(ROOTPATH)/hwpf/fapi2/include)
 $(call BUILD_WRAPPER)
+endif
