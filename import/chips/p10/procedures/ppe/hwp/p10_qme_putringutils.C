5674b4fa828ef5982d077a36a9cd3c69d25ce14c
Olsen <cmolsen@us.ibm.com>
2020-04-16 11:08:54 -0400

    Coreq avoidance reversal of commit 92684
    This commit removes the backwards compatibility code lines of
    commit 92684 which will allow an older image w/RS4 v4 rings to
    work with the codes of 92684 which use RS4 v5.
    
    This commit also introduces a systematic check of all rings that
    are retrieved from and appended to ringSections to ensure they
    match the code's RS4 version and have correctly programmed
    ORIGination bits set in RS4->iv_type.
    
    Key_Cronus_Test=XIP_REGRESS_SBE_QME
    
    Change-Id: I47d0deadbe90052395d970660eb50f24afe4582d
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/95188
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: Cronus HW CI <cronushw-ci+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Claus M Olsen <cmolsen@us.ibm.com>

--- import/chips/p10/procedures/ppe/hwp/p10_qme_putringutils.C
+++ import/chips/p10/procedures/ppe/hwp/p10_qme_putringutils.C
@@ -622,9 +622,6 @@ fapi2::ReturnCode p10_putRingUtils(
         FAPI_INF( "Ring Id is 0x%04x magic 0x%04x ring size 0x%04x",
                   (uint16_t)l_ringId , (uint32_t) rev_16(l_rs4Header->iv_magic), (uint16_t)l_ringSize);
 
-//CMO-20200410: Temp fix to avoid coreq
-if (l_rs4Header->iv_version == RS4_VERSION)
-{
         // Determine override/flush status
         if ( ( l_rs4Header->iv_type & RS4_IV_TYPE_SCAN_MASK ) == RS4_IV_TYPE_SCAN_OVRD )
         {
@@ -646,31 +643,6 @@ if (l_rs4Header->iv_version == RS4_VERSION)
             IOTA_PANIC(QME_STOP_PUTRING_HEADER_ERROR);
         #endif
         }
-}
-else
-{
-  // Determine override/flush status
-  if ( ( l_rs4Header->iv_type & RS4_IV_TYPE_SCAN_MASK_V4 ) == RS4_IV_TYPE_SCAN_OVRD_V4 )
-  {
-    l_bOverride = true;
-    PKTRACE( "Override Ring" );
-  }
-  else if ( ( l_rs4Header->iv_type & RS4_IV_TYPE_SCAN_MASK_V4 ) == RS4_IV_TYPE_SCAN_FLUSH_V4 )
-  {
-    l_bOverride = false;
-  }
-  else // Violation of decompression engine rules
-  {
-    FAPI_INF("The iv_type Ovrd bits MUST be either 0x%02x (override) or 0x%02x"
-             " (flush). However, the [masked] iv_type = 0x%02x",
-             RS4_IV_TYPE_SCAN_OVRD_V4,
-             RS4_IV_TYPE_SCAN_FLUSH_V4,
-             (l_rs4Header->iv_type & RS4_IV_TYPE_SCAN_MASK_V4));
-#ifndef __UNIT_TEST_
-  IOTA_PANIC(QME_STOP_PUTRING_HEADER_ERROR);
-#endif
-  }
-}
 
         // Get scan region and type value
         l_scanRegion    =   decodeScanRegionData( i_target, rev_32( l_rs4Header->iv_scanAddr ), l_ringId );
