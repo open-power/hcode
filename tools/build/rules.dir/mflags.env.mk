28f7134c04fb5cdf4d607a43a99e8de99d4504a1
Sumit Kumar <sumit_kumar@in.ibm.com>
2020-06-18 13:03:20 -0500

    Revert commit 98677-Auto mirror fix to EKB commit 90434
    Change-Id: I4ddd2bf60ef19f1975da5e09fae2da0e82375512

--- tools/build/rules.dir/mflags.env.mk
+++ tools/build/rules.dir/mflags.env.mk
@@ -39,24 +39,14 @@ endif
 
 # Location of the cross-compiler toolchain.
 UNAME = $(shell uname)
+ifneq ($(wildcard /opt/rh/devtoolset-8/root/usr/bin),)
+__EKB_PREFIX?=/opt/rh/devtoolset-8/root/usr/bin/
+else
+__EKB_PREFIX?=/opt/rh/devtoolset-2/root/usr/bin/
+endif
 
 ifeq ($(UNAME),AIX)
 __EKB_PREFIX=/opt/xsite/contrib/bin/
-else
-RHEL_VER = $(shell lsb_release -sr)
-ifeq ($(word 1, $(subst ., ,$(RHEL_VER))), 6)
-ifeq ($(wildcard /opt/rh/devtoolset-2/root/usr/bin),)
-$(error devtoolset-2 is not installed on RHEL $(RHEL_VER))
-endif
-__EKB_PREFIX?=/opt/rh/devtoolset-2/root/usr/bin/
-else ifeq ($(word 1, $(subst ., ,$(RHEL_VER))), 7)
-ifeq ($(wildcard /opt/rh/devtoolset-8/root/usr/bin),)
-$(error devtoolset-8 is not installed on RHEL $(RHEL_VER))
-endif
-__EKB_PREFIX?=/opt/rh/devtoolset-8/root/usr/bin/
-else
-$(error RHEL Version $RHEL_VER not supported)
-endif
 endif
 
 HOST_PREFIX?=$(__EKB_PREFIX)
