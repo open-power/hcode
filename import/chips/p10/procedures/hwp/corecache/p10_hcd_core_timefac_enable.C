44e8b7e0c7e3e963240b605ffd8d54f7e059222b
Prem Shanker Jha <premjha2@in.ibm.com>
2020-06-26 14:17:25 -0500

    HW534619: tfac-selfrestore workarounds and tfac tod fix
    Change-Id: I98fab35f11f417de085aa49a0ba6bc95069a94c5
    Original-Change-Id: Ia19dddb400eae81880fa6e9e77c992bf1b80ff07
    RTC: 256616
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/99333
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: RANGANATHPRASAD G. BRAHMASAMUDRA <prasadbgr@in.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: DAVID A KING <kingboy@us.ibm.com>

---
+++ import/chips/p10/procedures/hwp/corecache/p10_hcd_core_timefac_enable.C
@@ -0,0 +1,167 @@
+/* IBM_PROLOG_BEGIN_TAG                                                   */
+/* This is an automatically generated prolog.                             */
+/*                                                                        */
+/* $Source: import/chips/p10/procedures/hwp/corecache/p10_hcd_core_timefac_enable.C $ */
+/*                                                                        */
+/* OpenPOWER EKB Project                                                  */
+/*                                                                        */
+/* COPYRIGHT 2018,2020                                                    */
+/* [+] International Business Machines Corp.                              */
+/*                                                                        */
+/*                                                                        */
+/* Licensed under the Apache License, Version 2.0 (the "License");        */
+/* you may not use this file except in compliance with the License.       */
+/* You may obtain a copy of the License at                                */
+/*                                                                        */
+/*     http://www.apache.org/licenses/LICENSE-2.0                         */
+/*                                                                        */
+/* Unless required by applicable law or agreed to in writing, software    */
+/* distributed under the License is distributed on an "AS IS" BASIS,      */
+/* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or        */
+/* implied. See the License for the specific language governing           */
+/* permissions and limitations under the License.                         */
+/*                                                                        */
+/* IBM_PROLOG_END_TAG                                                     */
+
+
+
+///
+/// @file  p10_hcd_core_timefac_enable.C
+/// @brief
+///
+
+
+// *HWP HWP Owner          : David Du         <daviddu@us.ibm.com>
+// *HWP Backup HWP Owner   : Greg Still       <stillgs@us.ibm.com>
+// *HWP FW Owner           : Prem Shanker Jha <premjha2@in.ibm.com>
+// *HWP Team               : PM
+// *HWP Consumed by        : SBE:QME
+// *HWP Level              : 2
+
+
+//------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------
+
+#include "p10_hcd_core_timefac_enable.H"
+#include "p10_hcd_common.H"
+#include "p10_pm_hcd_flags.h"
+
+#ifdef __PPE_QME
+    #include "p10_ppe_c.H"
+    #include "p10_ppe_eq.H"
+    using namespace scomt::ppe_c;
+    using namespace scomt::ppe_eq;
+    #define EC_PC_TFX_SM 0x2002049b
+    #define EC_PC_TOD_READ 0x200204A3
+    #define QME_FLAGS_TOD_COMPLETE QME_FLAGS_TOD_SETUP_COMPLETE
+#else
+    #include "p10_scom_c.H"
+    #include "p10_scom_eq.H"
+    using namespace scomt::c;
+    using namespace scomt::eq;
+    #define QME_FLAGS_TOD_COMPLETE p10hcd::QME_FLAGS_TOD_SETUP_COMPLETE
+#endif
+
+
+//------------------------------------------------------------------------------
+// Constant Definitions
+//------------------------------------------------------------------------------
+
+enum P10_HCD_CORE_SHADOWS_ENABLE_CONSTANTS
+{
+    HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS      = 100000, // 10^5ns = 100us timeout
+    HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS        = 1000,   // 1us poll loop delay
+    HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_SIM_CYCLE    = 32000,  // 32k sim cycle delay
+};
+
+//------------------------------------------------------------------------------
+// Procedure: p10_hcd_core_timefac_enable
+//------------------------------------------------------------------------------
+
+fapi2::ReturnCode
+p10_hcd_core_timefac_enable(
+    const fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > & i_target)
+{
+    fapi2::buffer<buffer_t> l_mmioData            = 0;
+    uint32_t                l_timeout             = 0;
+    uint32_t                l_tfcsr_errors        = 0;
+    fapi2::Target < fapi2::TARGET_TYPE_CORE | fapi2::TARGET_TYPE_MULTICAST > l_mc_or = i_target;//default OR
+
+    FAPI_INF(">>p10_hcd_core_timefac_enable");
+
+    fapi2::Target < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST, fapi2::MULTICAST_AND > l_eq_target =
+        i_target.getParent < fapi2::TARGET_TYPE_EQ | fapi2::TARGET_TYPE_MULTICAST > ();
+
+    FAPI_TRY(HCD_GETMMIO_Q( l_eq_target, QME_FLAGS_RW, l_mmioData ) );
+
+    if ( MMIO_GET( QME_FLAGS_TOD_COMPLETE ) == 1 )
+    {
+        // HW534619 DD1 workaround move to self restore
+        FAPI_DBG("DD1 HW534619: Write 0 to TOD_READ");
+        FAPI_TRY( HCD_PUTSCOM_C( i_target, EC_PC_TOD_READ, 0 ) );
+
+        FAPI_DBG("Assert XFER_START via PCR_TFCSR[0]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_TFCSR_WO_OR, MMIO_1BIT(0) ) );
+
+        FAPI_DBG("Wait on XFER_SENT_DONE via PCR_TFCSR[33]");
+        l_timeout = HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS /
+                    HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS;
+
+        do
+        {
+            FAPI_TRY( HCD_GETMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR), l_mmioData ) );
+
+            // use multicastAND to check 1
+            if( MMIO_GET(MMIO_LOWBIT(33)) == 1 )
+            {
+                break;
+            }
+
+            fapi2::delay(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_HW_NS,
+                         HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_DELAY_SIM_CYCLE);
+        }
+        while( (--l_timeout) != 0 );
+
+        FAPI_ASSERT((l_timeout != 0),
+                    fapi2::SHADOW_ENA_XFER_SENT_DONE_TIMEOUT()
+                    .set_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS(HCD_SHADOW_ENA_XFER_SENT_DONE_POLL_TIMEOUT_HW_NS)
+                    .set_QME_TFCSR(l_mmioData)
+                    .set_CORE_TARGET(i_target),
+                    "ERROR: Shadow Enable Xfer Sent Done Timeout");
+
+        FAPI_DBG("Check INCOMING/RUNTIME/STATE_ERR == 0 via PCR_TFCSR[34-36]");
+        FAPI_TRY( HCD_GETMMIO_C( l_mc_or, MMIO_LOWADDR(QME_TFCSR), l_mmioData ) );
+
+        MMIO_EXTRACT(MMIO_LOWBIT(34), 3, l_tfcsr_errors);
+
+        if( l_tfcsr_errors != 0 )
+        {
+            FAPI_DBG("Clear INCOMING/RUNTIME/STATE_ERR[%x] via PCR_TFCSR[34-36]", l_tfcsr_errors);
+            FAPI_TRY( HCD_PUTMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR_WO_CLEAR), MMIO_LOAD32L( BITS64SH(34, 3) ) ) );
+
+            FAPI_DBG("Assert TFAC_RESET via PCR_TFCSR[1]");
+            FAPI_TRY( HCD_PUTMMIO_C( i_target, QME_TFCSR_WO_OR, MMIO_1BIT(1) ) );
+
+            FAPI_DBG("Reset the core timefac to INACTIVE via PC.COMMON.TFX[1]");
+            FAPI_TRY( HCD_PUTSCOM_C( i_target, EC_PC_TFX_SM, BIT64(1) ) );
+        }
+
+        FAPI_ASSERT((l_tfcsr_errors == 0),
+                    fapi2::SHADOW_ENA_TFCSR_ERROR_CHECK_FAILED()
+                    .set_QME_TFCSR(l_tfcsr_errors)
+                    .set_CORE_TARGET(i_target),
+                    "ERROR: Shadow Disable TFCSR Error Check Failed");
+
+        FAPI_DBG("Drop XFER_SENT_DONE via PCR_TFCSR[33]");
+        FAPI_TRY( HCD_PUTMMIO_C( i_target, MMIO_LOWADDR(QME_TFCSR_WO_CLEAR), MMIO_1BIT( MMIO_LOWBIT(33) ) ) );
+    }
+    else
+    {
+        FAPI_INF("TOD not enabled.  Skipping enablement of TimeFac Shadow");
+    }
+
+fapi_try_exit:
+    FAPI_INF("<<p10_hcd_core_timefac_enable");
+    return fapi2::current_err;
+}
