a1cd95f8d4ad1752997dc97515a22ccd14593afd
Douglas Gilbert <dgilbert@us.ibm.com>
2021-03-09 11:20:25 -0600

    PPE42: Add _modsi3 32 bit signed modulus
    Change-Id: I3e8e586ea1a155b0eb22a1d55b4ca693739181b4
    Original-Change-Id: Ibf5e1b5190ea5952954fc37d5c86acef3f9ad6dd
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/112426
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Reviewed-by: YUE DU <daviddu@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>
    Reviewed-by: DAVID A KING <kingboy@us.ibm.com>

--- import/chips/p10/common/ppe/baselib/ppe42_math.c
+++ import/chips/p10/common/ppe/baselib/ppe42_math.c
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER EKB Project                                                  */
 /*                                                                        */
-/* COPYRIGHT 2016,2019                                                    */
+/* COPYRIGHT 2016,2021                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -147,6 +147,32 @@ int __divsi3(int _a, int _b)
     }
 
     int c = (int)__udivsi3((unsigned long)_a, (unsigned long)_b);
+
+    if(neg)
+    {
+        c = (~c) + 1;
+    }
+
+    return c;
+}
+
+int __modsi3(int _a, int _b)
+{
+    register unsigned long neg = 0;
+
+    if(_a & 0x80000000)
+    {
+        neg = !neg;
+        _a = (~_a) + 1;
+    }
+
+    if(_b & 0x80000000)
+    {
+        _b = (~_b) + 1;
+        neg = !neg;
+    }
+
+    int c = (int)__umodsi3((unsigned long)_a, (unsigned long)_b);
 
     if(neg)
     {
