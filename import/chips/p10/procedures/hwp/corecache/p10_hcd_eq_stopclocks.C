3539ebb9c035d87fd46fd63c42c896119496a0a0
Yue Du <daviddu@us.ibm.com>
2021-03-29 16:56:59 -0400

    Stopclocks: drop SRAM_ABIST_MODE_DC in eq_stopclocks
    Change-Id: I5b6cfa28c07a6108755634bc23f17ff31bfbb229
    Reviewed-on: http://rchgit01.rchland.ibm.com/gerrit1/113815
    Reviewed-by: Justin I Smith <justin.i.smith@ibm.com>
    Reviewed-by: NICHOLAS WESTFAHL <westfahl@us.ibm.com>
    Tested-by: Jenkins Server <pfd-jenkins+hostboot@us.ibm.com>
    Tested-by: PPE CI <ppe-ci+hostboot@us.ibm.com>
    Tested-by: FSP CI Jenkins <fsp-CI-jenkins+hostboot@us.ibm.com>
    Tested-by: Hostboot CI <hostboot-ci+hostboot@us.ibm.com>
    Reviewed-by: Gregory S Still <stillgs@us.ibm.com>

--- import/chips/p10/procedures/hwp/corecache/p10_hcd_eq_stopclocks.C
+++ import/chips/p10/procedures/hwp/corecache/p10_hcd_eq_stopclocks.C
@@ -5,7 +5,7 @@
 /*                                                                        */
 /* OpenPOWER EKB Project                                                  */
 /*                                                                        */
-/* COPYRIGHT 2018,2020                                                    */
+/* COPYRIGHT 2018,2021                                                    */
 /* [+] International Business Machines Corp.                              */
 /*                                                                        */
 /*                                                                        */
@@ -71,6 +71,13 @@ p10_hcd_eq_stopclocks(
     FAPI_TRY( HCD_PUTSCOM_Q( i_target, CPLT_CTRL1_WO_OR, SCOM_LOAD32H( HCD_CLK_REGION_ALL ) ) );
 
     FAPI_TRY( p10_hcd_corecache_clock_control( i_target, HCD_CLK_REGION_ALL, HCD_CLK_STOP ) );
+
+#ifndef __PPE_QME
+
+    FAPI_DBG("Drop TC_SRAM_ABIST_MODE_DC only for Scan via BIST[1]");
+    FAPI_TRY( putScom(i_target, BIST, 0));
+
+#endif
 
 fapi_try_exit:
 
